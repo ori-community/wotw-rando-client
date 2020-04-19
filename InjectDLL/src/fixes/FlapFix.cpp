431f917a871d36360a6918e2746477569d85a3f6
Xemsys <xemsys@gmail.com>
2020-04-19 22:43:12 +0200

    allow flap without glide and burrow/waterdash without dash

---
+++ InjectDLL/src/fixes/FlapFix.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "interception.h"
+#include "interceptionMacros.h"
+#include "dashFixes.h"
+
+const uint8_t glide = 14;
+INTERCEPT(17750640, void, SeinFeatherFlap__EnterAttack, (SeinFeatherFlap_o* this_ptr), {
+	auto abilities = this_ptr->CharacterState_m_sein->PlayerAbilities;
+	if(PlayerAbilities__HasAbility(abilities, glide)) {
+		SeinFeatherFlap__EnterAttack(this_ptr);
+	} else {
+		PlayerAbilities__SetAbility(abilities, glide, true);
+		SeinFeatherFlap__EnterAttack(this_ptr);
+		PlayerAbilities__SetAbility(abilities, glide, false);
+	}
+
+})
