ed2cf7fc2c28adfe62bc0570a32482dd433cf76a
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2020-06-11 08:09:41 +0100

    Cmake addendum (#23)
    * Make it so the install project puts all output files into C:/moon
    
    * Modified cmake to conform to previous project setup.

--- gen-win64.bat
+++ gen-win64.bat
@@ -6,6 +6,10 @@ cd build
 mkdir win64
 cd win64
 cmake -DARCH=x64 -G "Visual Studio 16 2019" ..\..
+echo f | xcopy /y ..\..\DllExport.bat .\DllExport.bat
+echo f | xcopy /y ..\..\.net.dllexport.targets .\.net.dllexport.targets
+xcopy /y /s ..\..\packages .\packages
+DllExport.bat -action Recover -sln-file WotWRandomizer.sln
 cd ..
 cd ..
 pause
