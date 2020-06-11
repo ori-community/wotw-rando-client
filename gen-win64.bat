c401a7e118ad047a234ba11f0808137a83fbccfb
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2020-06-11 08:57:59 +0100

    Add ilmerge again and remove unused files. (#24)

--- gen-win64.bat
+++ gen-win64.bat
@@ -5,11 +5,17 @@ mkdir build
 cd build
 mkdir win64
 cd win64
+
 cmake -DARCH=x64 -G "Visual Studio 16 2019" ..\..
+
 echo f | xcopy /y ..\..\DllExport.bat .\DllExport.bat
 echo f | xcopy /y ..\..\.net.dllexport.targets .\.net.dllexport.targets
-xcopy /y /s ..\..\packages .\packages
-DllExport.bat -action Recover -sln-file WotWRandomizer.sln
+
+rem We need to call DllExport using cmd to prevent the batch from terminating.
+cmd /c DllExport.bat -action Recover -sln-file WotWRandomizer.sln
+
+xcopy /i /y /s ..\..\packages .\packages
+
 cd ..
 cd ..
 pause
