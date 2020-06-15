b061ac996feca54fbc23ce4ef3d9b785acf223a8
Xemsys <xemsys@gmail.com>
2020-06-15 20:32:47 +0200

    All shopkeepers have agreed to commence difficulty-independent price-fixing

---
+++ projects/InjectDLL/features/difficulty.cpp
@@ -0,0 +1,9 @@
+#include <pch.h>
+#include <interception_macros.h>
+#include <dll_main.h>
+
+INTERCEPT(10061888, GameController_GameModeBalanceSettings_o*, GameController__get_CurrentGameModeBalanceSettings, (GameController_o* this_ptr), {
+    auto result = GameController__get_CurrentGameModeBalanceSettings(this_ptr);
+    result->ExperienceOrbCostMultiplier = this_ptr->NormalModeBalanceSettings->ExperienceOrbCostMultiplier;
+    return result;
+})
