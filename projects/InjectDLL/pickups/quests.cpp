eb518bfbacd059121bf716e44c12a18e2d65e782
Xemsys <xemsys@gmail.com>
2020-06-15 19:30:21 +0200

    Remove vanilla Ore/Keystone/SpiritLight rewards from quests and trials

---
+++ projects/InjectDLL/pickups/quests.cpp
@@ -0,0 +1,14 @@
+#include <pch.h>
+#include <interception_macros.h>
+#include <dll_main.h>
+
+#include "pickups.h"
+
+//noop only - reward triggers on uberstate change.
+INTERCEPT(11712560, void, QuestsController__ApplyReward, (QuestsController_o * this_ptr, QuestReward_o * reward),{})
+INTERCEPT(11697904, void, QuestNodeWisps__ApplyReward, (QuestNodeWisps_o* this_ptr),{
+    collecting_pickup = true;
+    QuestNodeWisps__ApplyReward(this_ptr);
+    collecting_pickup = false;      
+})
+INTERCEPT(12110000, void, RaceHandler__ApplyReward, (RaceHandler_o* this_ptr),{})
