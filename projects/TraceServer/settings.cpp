39027607ebdbc59b943c7460744a0b577783a835
BuildTools <badulf96@gmail.com>
2020-06-17 12:18:07 +0100

    Various fixes and improvements.  - Renamed LayoutPlanner to Layout  - Added a Settings struct into TraceServer that handles ini files.  - Added UI for modifying Randomizer settings in Trace Server.  - Addd CMakeLists.txt to all projects output so they can be modified in Visual Studio  - Added new regen batch file that automatically deletes the build folder before generating the projects.  - Fixed a bug in the Layout logic where we exited out early at the wrong time.

---
+++ projects/TraceServer/settings.cpp
@@ -0,0 +1,188 @@
+#include <settings.h>
+
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <xstring>
+
+#include <assert.h>
+#include <windows.h>
+
+void load_settings_from_file(IniSettings& settings)
+{
+    if (!std::filesystem::exists(settings.path))
+        return;
+
+    for (auto& option : settings.options)
+    {
+        switch (option.type)
+        {
+        case IniVarType::Bool:
+        {
+            auto length = GetPrivateProfileString(
+                option.section.c_str(),
+                option.name.c_str(),
+                option.value.b ? "true" : "false",
+                option.value.s.data(),
+                option.value.s.size(),
+                settings.path.c_str()
+            );
+
+            std::string value{ option.value.s.data(), length };
+            std::transform(value.begin(), value.end(), value.begin(), [](auto c) { return std::tolower(c); });
+            option.value.b = value == std::string{ "true" };
+            break;
+        }
+        case IniVarType::Int:
+        {
+            option.value.i = GetPrivateProfileInt(
+                option.section.c_str(),
+                option.name.c_str(),
+                option.value.i,
+                settings.path.c_str()
+            );
+            break;
+        }
+        case IniVarType::Float:
+        {
+            auto length = GetPrivateProfileString(
+                option.section.c_str(),
+                option.name.c_str(),
+                std::to_string(option.value.f).c_str(),
+                option.value.s.data(),
+                option.value.s.size(),
+                settings.path.c_str()
+            );
+
+            std::string value{ option.value.s.data(), length };
+            option.value.f = std::stof(value);
+            break;
+        }
+        case IniVarType::String:
+        {
+            auto length = GetPrivateProfileString(
+                option.section.c_str(),
+                option.name.c_str(),
+                option.value.s.data(),
+                option.value.s.data(),
+                option.value.s.size(),
+                settings.path.c_str()
+            );
+
+            break;
+        }
+        default:
+            assert(false);
+        }
+    }
+}
+
+void save_settings_to_file(IniSettings& settings)
+{
+    if (!std::filesystem::exists(settings.path))
+        std::fstream().open(settings.path);
+
+    for (auto& option : settings.options)
+    {
+        std::string value;
+        switch (option.type)
+        {
+        case IniVarType::Bool:
+            value = option.value.b ? "true" : "false";
+            break;
+        case IniVarType::Int:
+            value = std::to_string(option.value.i);
+            break;
+        case IniVarType::Float:
+            value = std::to_string(option.value.f);
+            break;
+        case IniVarType::String:
+            value = option.value.s.data();
+            break;
+        default:
+            assert(false);
+        }
+
+        WritePrivateProfileString(
+            option.section.c_str(),
+            option.name.c_str(),
+            value.c_str(),
+            settings.path.c_str()
+        );
+    }
+}
+
+IniOption* find_option(IniSettings& settings, std::string const& section, std::string const& name)
+{
+    auto it = std::find_if(settings.options.begin(), settings.options.end(), [&](auto const& option) -> bool {
+        return option.section == section && option.name == name;
+    });
+
+    if (it != settings.options.end())
+        return &*it;
+
+    return nullptr;
+}
+
+IniSettings create_randomizer_settings()
+{
+    IniSettings file;
+    file.path = "C:\\moon\\settings.ini";
+
+    IniOption option;
+    option.section = "Paths";
+    option.type = IniVarType::String;
+
+    option.name = "Steam";
+    strcpy_s(option.value.s.data(), option.value.s.size(), "C:\\Program Files (x86)\\Steam\\steam.exe");
+    file.options.push_back(option);
+
+    option.section = "Flags";
+    option.type = IniVarType::Bool;
+
+    option.name = "UseWinStore";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "SkipUpdate";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "MuteInjectLogs";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "MuteCSLogs";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "ShowShortCutscenes";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "ShowLongCutscenes";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "CursorLock";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "InvertSwim";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "Dev";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "TraceEnabled";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    option.name = "TracePingingDisabled";
+    option.value.b = false;
+    file.options.push_back(option);
+
+    return file;
+}
---
+++ projects/TraceServer/settings.h
@@ -0,0 +1,40 @@
+#include <string>
+#include <array>
+#include <vector>
+
+enum class IniVarType
+{
+    Bool,
+    Int,
+    Float,
+    String
+};
+
+union IniVariant
+{
+    bool b;
+    int i;
+    float f;
+    std::array<char, 128> s;
+};
+
+struct IniOption
+{
+    std::string section;
+    std::string name;
+    IniVarType type;
+    IniVariant value;
+};
+
+struct IniSettings
+{
+    std::string path;
+    std::vector<IniOption> options;
+};
+
+IniSettings create_randomizer_settings();
+
+void load_settings_from_file(IniSettings& settings);
+void save_settings_to_file(IniSettings& settings);
+
+IniOption* find_option(IniSettings& settings, std::string const& section, std::string const& name);
