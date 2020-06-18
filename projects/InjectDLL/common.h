1b1d13b809dc1e69050975ddb40988d115420352
BuildTools <badulf96@gmail.com>
2020-06-18 17:50:11 +0100

    Fix ore count in inventory. Some minor cmake changes and added missing pragma once.

--- projects/InjectDLL/common.h
+++ projects/InjectDLL/common.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <constants.h>
 #include <string>
 
+void send_trace(MessageType type, int level, std::string const& group, std::string const& message);
 void log(std::string);
 void debug(std::string);
 void error(std::string);
--- projects/InjectDLL/pickups/ore.cpp
+++ projects/InjectDLL/pickups/ore.cpp
@@ -1,12 +1,97 @@
 #include <pch.h>
+#include <pe_module.h>
+#include <common.h>
 #include <interception_macros.h>
 #include <pickups/pickups.h>
 
+extern InjectDLL::PEModule* csharp_lib;
+
+// Taken from dump.cs
+enum class WorldMapIconType : int32_t
+{
+    value__,
+    Keystone = 0,
+    Mapstone = 1,
+    BreakableWall = 2,
+    BreakableWallBroken = 3,
+    StompableFloor = 4,
+    StompableFloorBroken = 5,
+    EnergyGateTwo = 6,
+    EnergyGateOpen = 7,
+    KeystoneDoorFour = 8,
+    KeystoneDoorOpen = 9,
+    AbilityPedestal = 10,
+    HealthUpgrade = 11,
+    EnergyUpgrade = 12,
+    SavePedestal = 13,
+    AbilityPoint = 14,
+    KeystoneDoorTwo = 15,
+    Invisible = 16,
+    Experience = 17,
+    MapstonePickup = 18,
+    EnergyGateTwelve = 19,
+    EnergyGateTen = 20,
+    EnergyGateEight = 21,
+    EnergyGateSix = 22,
+    EnergyGateFour = 23,
+    SpiritShard = 24,
+    NPC = 25,
+    QuestItem = 26,
+    ShardSlotUpgrade = 27,
+    Teleporter = 28,
+    Ore = 29,
+    HealthFragment = 33,
+    EnergyFragment = 34,
+    Seed = 35,
+    QuestStart = 30,
+    QuestEnd = 31,
+    RaceStart = 32,
+    RaceEnd = 36,
+    Eyestone = 37,
+    WatermillDoor = 40,
+    TempleDoor = 41,
+    SmallDoor = 42,
+    Shrine = 43,
+    Loremaster = 50,
+    Weaponmaster = 51,
+    Gardener = 52,
+    Mapmaker = 53,
+    Shardtrader = 54,
+    Wanderer = 55,
+    Treekeeper = 56,
+    Builder = 57,
+    Kwolok = 58,
+    Statistician = 59,
+    CreepHeart = 60,
+    Miner = 61,
+    Spiderling = 62,
+    Moki = 63,
+    MokiBrave = 64,
+    MokiAdventurer = 65,
+    MokiArtist = 66,
+    MokiDarkness = 67,
+    MokiFashionable = 68,
+    MokiFisherman = 69,
+    MokiFrozen = 70,
+    MokiKwolokAmulet = 71,
+    MokiSpyglass = 72,
+    Ku = 73,
+    IceFisher = 74,
+    Siira = 75
+};
+
+INTERCEPT(4093520, int32_t, GameWorld__GetCollectedIconTypeCount, (GameWorld_o* this_ptr, WorldMapIconType_o type), {
+    auto value = GameWorld__GetCollectedIconTypeCount(this_ptr, type);
+    if (type.value__ == static_cast<int32_t>(WorldMapIconType::Ore))
+        value = csharp_lib->call<int>("OreCount", type);
+
+    return value;
+});
 
 INTERCEPT(5814720, void, SeinPickupProcessor__OnCollectOrePickup, (SeinPickupProcessor_o* this_ptr, OrePickup_o* orePickup), {	
 	collecting_pickup = true;
 	SeinPickupProcessor__OnCollectOrePickup(this_ptr, orePickup);
-  collecting_pickup = false;
+    collecting_pickup = false;
 });
 
 INTERCEPT(8453568, void, SeinLevel__set_Ore, (SeinLevel_o* this_ptr, int32_t value), {
