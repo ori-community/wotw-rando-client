dd6404f8dc24e100a3898949aefd625c7e4f3ed6
Eiko <worst.mirari@gmail.com>
2020-06-23 13:04:18 -0700

    some tweaks to new interop (generics ^_^)

--- projects/RandoMainDLL/InterOp.cs
+++ projects/RandoMainDLL/InterOp.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using RandoMainDLL.Memory;
 using System.Linq;
