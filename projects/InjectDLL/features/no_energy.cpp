2ca55dbfeefc5a8cb6604eb6b347fb92bb231063
BuildTools <badulf96@gmail.com>
2020-06-29 20:50:33 +0100

    added no energy cost setting

---
+++ projects/InjectDLL/features/no_energy.cpp
@@ -0,0 +1,21 @@
+#include <interception_macros.h>
+#include <dev_commands.h>
+
+namespace
+{
+    bool no_energy = false;
+
+    INTERCEPT(9451104, float, SeinEnergy__get_EnergyCostMultiplier, (SeinEnergy_o* this_ptr)) {
+        if (no_energy)
+            return 0.f;
+
+        return SeinEnergy__get_EnergyCostMultiplier(this_ptr);
+    }
+
+    void initialize_no_energy()
+    {
+        no_energy = csharp_bridge::check_ini("NoEnergyCost");
+    }
+
+    CALL_ON_INIT(initialize_no_energy);
+}
