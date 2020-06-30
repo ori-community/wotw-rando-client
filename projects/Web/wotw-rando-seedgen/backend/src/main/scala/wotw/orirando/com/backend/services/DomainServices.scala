6df5b507546ce6d851319780b88e080bd7a7e31d
Eiko <worst.mirari@gmail.com>
2020-06-30 12:17:13 -0700

    removed unused files

--- projects/Web/wotw-rando-seedgen/backend/src/main/scala/wotw/orirando/com/backend/services/DomainServices.scala
+++
@@ -1,8 +0,0 @@
-package wotw.orirando.com.backend.services
-
-/** Container for all services used to implicitly pass services to endpoints. */
-class DomainServices(implicit
-  val authService: AuthService,
-  val chatService: ChatService,
-  val rpcClientsService: RpcClientsService
-)
--- projects/Web/wotw-rando-seedgen/frontend/src/main/scala/wotw/orirando/com/frontend/services/rpc/RPCService.scala
+++
@@ -1,9 +0,0 @@
-package wotw.orirando.com.frontend.services.rpc
-
-import wotw.orirando.com.shared.rpc.client.MainClientRPC
-import wotw.orirando.com.shared.rpc.client.chat.ChatNotificationsRPC
-
-class RPCService(notificationsCenter: NotificationsCenter) extends MainClientRPC {
-  override val chat: ChatNotificationsRPC =
-    new ChatService(notificationsCenter.msgListeners, notificationsCenter.connectionsListeners)
-}
