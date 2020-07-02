db875b4860223b1e0d865280f0c445ec4cfcc12f
BuildTools <badulf96@gmail.com>
2020-07-02 08:46:37 +0100

    added 2 missing sets of states for clean water.

--- projects/InjectDLL/features/water.cpp
+++ projects/InjectDLL/features/water.cpp
@@ -13,6 +13,16 @@ namespace
         if (stateGUID == 569716315 || stateGUID == 2044614461)
             stateGUID = csharp_bridge::water_cleansed() ? 2044614461 : 569716315;
 
+        // notClean 135459242
+        // Clean 2010339656
+        if (stateGUID == 135459242 || stateGUID == 2010339656)
+            stateGUID = csharp_bridge::water_cleansed() ? 2010339656 : 135459242;
+
+        // corrupted 742737118
+        // clean 669048353
+        if (stateGUID == 742737118 || stateGUID == 669048353)
+            stateGUID = csharp_bridge::water_cleansed() ? 669048353 : 742737118;
+
         NewSetupStateController__ApplyKnownState(this_ptr, stateGUID, context);
     }
 }
