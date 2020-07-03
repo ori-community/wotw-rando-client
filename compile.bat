2612c6934475146edf524fddd2b520ed786d2683
Eiko <worst.mirari@gmail.com>
2020-07-03 14:45:06 -0700

    add ahk compiler to repo

--- compile.bat
+++ compile.bat
@@ -2,6 +2,6 @@ copy "projects\SeedGen\loc_data.csv" "C:\moon\loc_data.csv" /Y
 copy "projects\SeedGen\areas.wotw" "C:\moon\areas.wotw" /Y
 copy "VERSION" "C:\moon\VERSION" /Y
 
-"C:/Program Files/AutoHotkey/Compiler/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
-"C:/Program Files/AutoHotkey/Compiler/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
-"C:/Program Files/AutoHotkey/Compiler/Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:/moon/WotwRando.exe"
+"ext/ahk/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
+"ext/ahk/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
+"ext/ahk/Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:/moon/WotwRando.exe"
