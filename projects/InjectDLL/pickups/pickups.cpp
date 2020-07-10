1a9fba976d8628a213a89780b08ea0bd7a819b58
Eiko <worst.mirari@gmail.com>
2020-07-10 00:46:42 -0700

    Revert "remove ore message"
    This reverts commit 40b678d595de7faaf51b3e12654b712779fb3103.

--- projects/InjectDLL/pickups/pickups.cpp
+++ projects/InjectDLL/pickups/pickups.cpp
@@ -1,6 +1,5 @@
 #include <interception_macros.h>
 #include <dll_main.h>
-#include <common.h>
 #include <pickups/pickups.h>
 
 bool collecting_pickup = false;
@@ -53,20 +52,6 @@ void shake_ore()
         SeinUI_ShakeSeeds(get_ui()->static_fields->SeinUI);
 }
 
-IL2CPP_BINDING(, MessageProvider, app::String__Array*, GetAllMessages, (app::MessageProvider* this_ptr));
-IL2CPP_INTERCEPT(, MessageControllerB, app::MessageBox*, ShowPickupMessage, (app::MessageControllerB* this_ptr, app::MessageProvider* message_provider, app::PickupContext* context, bool lockInput)) {
-  if (context->fields.PickupType == app::PickupMessageIcon_PickupType__Enum_QuestItem) {
-    auto arr = MessageProvider_GetAllMessages(context->fields.Name);
-    if (arr->max_length > 0)
-    {
-      auto name = convert_csstring(arr->vector[0]);
-      if (name == "Gorlek Ore")
-        return nullptr;
-    }
-  }
-
-  return MessageControllerB_ShowPickupMessage(this_ptr, message_provider, context, lockInput);
-}
 IL2CPP_INTERCEPT(, SeinPickupProcessor, void, PerformPickupSequence, (app::SeinPickupProcessor* thisPtr, app::SeinPickupProcessor_CollectableInfo* info)) {
 
 }
