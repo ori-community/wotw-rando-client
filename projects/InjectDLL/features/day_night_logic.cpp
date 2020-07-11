24886b699e17ca0cb0b9a5ce13f8efb25cfcaadf
BuildTools <badulf96@gmail.com>
2020-07-11 04:26:52 +0100

    Fixed enemies spawning like it was night time if you didnt have sword.

--- projects/InjectDLL/features/day_night_logic.cpp
+++ projects/InjectDLL/features/day_night_logic.cpp
@@ -4,6 +4,13 @@
 
 namespace
 {
+    IL2CPP_INTERCEPT(, SeinAbilityCondition, bool, Validate, (app::SeinAbilityCondition* this_ptr, app::IContext* context)) {
+        if (this_ptr->fields.Ability == app::AbilityType__Enum_Sword)
+            return true;
+        else
+            return SeinAbilityCondition_Validate(this_ptr, context);
+    }
+
     IL2CPP_INTERCEPT(, HasAbilityCondition, bool, Validate, (app::HasAbilityCondition* this_ptr, app::IContext* context)) {
         if (this_ptr->fields.AbilityType == app::AbilityType__Enum_Sword)
             return true;
@@ -40,9 +47,31 @@ namespace
         uber_states::register_applier_redirect(787945376, 1001861749);
         uber_states::register_applier_redirect(1361521887, -1375966924);
         uber_states::register_applier_redirect(-480342150, 907153171);
-
+        
         // howl: notDefeated -> defeated
         uber_states::register_applier_redirect(1234021711, 620462779);
+    }
+
+    IL2CPP_INTERCEPT(, ActivationBasedOnCondition, void, Awake, (app::ActivationBasedOnCondition* this_ptr)) {
+        ActivationBasedOnCondition_Awake(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SwampNightDayTransition, bool, DayTimeCondition, (app::SwampNightDayTransition* this_ptr)) {
+        return true;
+    }
+
+    bool override_has_ability = false;
+    IL2CPP_INTERCEPT(, SwampNightDayTransition, void, UpdateStateBasedOnCondition, (app::SwampNightDayTransition* this_ptr)) {
+        override_has_ability = true;
+        SwampNightDayTransition_UpdateStateBasedOnCondition(this_ptr);
+        override_has_ability = false;
+    }
+
+    IL2CPP_INTERCEPT(, PlayerAbilities, bool, HasAbility, (app::PlayerAbilities* this_ptr, app::AbilityType__Enum ability)) {
+        if (override_has_ability && ability == app::AbilityType__Enum_Sword)
+            return true;
+        else
+            return PlayerAbilities_HasAbility(this_ptr, ability);
     }
 
     CALL_ON_INIT(initialize_day_night_logic);
