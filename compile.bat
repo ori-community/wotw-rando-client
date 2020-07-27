01dc07f4020b192cddd3162cdf6ea11a74a02a79
Eiko <worst.mirari@gmail.com>
2020-07-27 14:29:01 -0700

    compile script now builds seedgen automatically

--- compile.bat
+++ compile.bat
@@ -1,6 +1,12 @@
-copy "projects\SeedGen\loc_data.csv" "C:\moon\loc_data.csv" /Y
-copy "projects\SeedGen\areas.wotw" "C:\moon\areas.wotw" /Y
-copy "VERSION" "C:\moon\VERSION" /Y
+copy "projects/SeedGen/loc_data.csv" "C:/moon/loc_data.csv" /Y
+copy "projects/SeedGen/areas.wotw" "C:/moon/areas.wotw" /Y
+copy "VERSION" "C:/moon/VERSION" /Y
+
+cd "projects/SeedGen"
+
+call sbt assembly
+
+cd "../.."
 
 "ext/ahk/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
 "ext/ahk/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
