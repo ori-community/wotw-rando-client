a307b2d6ca1f7d28f1dd42ff5ff64665d64695a4
Eiko <worst.mirari@gmail.com>
2020-07-30 01:02:30 -0700

    just some misc neat stuff for me really

--- compile.bat
+++ compile.bat
@@ -1,13 +1,27 @@
+
+cd "%~dp0"
+
+:: Either change paths or build yourself and run with nobuild
+if NOT "%1"=="nobuild" (
+	"C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MsBuild.exe" -targets -p:Configuration=MinSizeRel -p:Platform=x64 C:\Users\WorstMirari\Documents\GitHub\OriWotwRandomizerClient\build\win64\WotWRandomizer.sln
+	IF NOT %ERRORLEVEL%==0 (
+		Title BUILD FAILED
+		exit 1
+	)
+)
+
 copy "projects/SeedGen/loc_data.csv" "C:/moon/loc_data.csv" /Y
 copy "projects/SeedGen/areas.wotw" "C:/moon/areas.wotw" /Y
 copy "VERSION" "C:/moon/VERSION" /Y
 
-cd "projects/SeedGen"
+if NOT "%1"=="buildonly" (
+	cd "projects/SeedGen"
 
-call sbt assembly
+	call sbt assembly
 
-cd "../.."
+	cd "../.."
 
-"ext/ahk/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
-"ext/ahk/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
-"ext/ahk/Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:/moon/WotwRando.exe"
+	"ext/ahk/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
+	"ext/ahk/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
+	"ext/ahk/Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:/moon/WotwRando.exe"
+)
