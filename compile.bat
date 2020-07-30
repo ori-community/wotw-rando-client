0e8e40cffbce27acfb67a7be9308e8e71e67371f
Eiko <worst.mirari@gmail.com>
2020-07-30 03:18:22 -0700

    flag for wolf to release with

--- compile.bat
+++ compile.bat
@@ -1,6 +1,14 @@
 
 cd "%~dp0"
-
+if "%1"=="compileonly" (
+	copy "projects/SeedGen/loc_data.csv" "C:/moon/loc_data.csv" /Y
+	copy "projects/SeedGen/areas.wotw" "C:/moon/areas.wotw" /Y
+	copy "VERSION" "C:/moon/VERSION" /Y
+	"ext/ahk/Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:/moon/RandoSettings.exe"
+	"ext/ahk/Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:/moon/ItemTracker.exe"
+	"ext/ahk/Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:/moon/WotwRando.exe"
+	exit 0
+)
 :: Either change paths or build yourself and run with nobuild
 if NOT "%1"=="nobuild" (
 	"C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MsBuild.exe" -targets -p:Configuration=MinSizeRel -p:Platform=x64 C:\Users\WorstMirari\Documents\GitHub\OriWotwRandomizerClient\build\win64\WotWRandomizer.sln
