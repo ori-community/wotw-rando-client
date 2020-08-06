0cb89b840cf9691b4744999c0a79e52594f53855
Eiko <worst.mirari@gmail.com>
2020-08-05 20:08:55 -0700

    sbt assembly before compile so it finishes in time

--- compile.bat
+++ compile.bat
@@ -11,6 +11,14 @@ if "%1"=="compileonly" (
 	".\ext\ahk\Ahk2Exe.exe" /in ".\WotwRando.ahk" /icon ".\WotwRando.ico" /out "C:\moon\WotwRando.exe"
 	exit 0
 )
+if NOT "%1"=="buildonly" (
+	cd "projects\SeedGen"
+
+	call sbt assembly
+
+	cd "..\.."
+)
+
 :: Either change paths or build yourself and run with nobuild
 if NOT "%1"=="nobuild" (
 	"C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\MSBuild\Current\Bin\MsBuild.exe" -targets -p:Configuration=MinSizeRel -p:Platform=x64 C:\Users\WorstMirari\Documents\GitHub\OriWotwRandomizerClient\build\win64\WotWRandomizer.sln
@@ -26,12 +34,6 @@ copy "projects\SeedGen\areas.wotw" "C:\moon\areas.wotw" /Y
 copy "VERSION" "C:\moon\VERSION" /Y
 
 if NOT "%1"=="buildonly" (
-	cd "projects\SeedGen"
-
-	call sbt assembly
-
-	cd "..\.."
-
 	"ext\ahk\Ahk2Exe.exe" /in "RandoSettings.ahk" /icon "WotwRando.ico" /out "C:\moon\RandoSettings.exe"
 	"ext\ahk\Ahk2Exe.exe" /in "projects/AutoTracker/OriAutoTracker.ahk" /icon "WotwRando.ico" /out "C:\moon\ItemTracker.exe"
 	"ext\ahk\Ahk2Exe.exe" /in "WotwRando.ahk" /icon "WotwRando.ico" /out "C:\moon\WotwRando.exe"
