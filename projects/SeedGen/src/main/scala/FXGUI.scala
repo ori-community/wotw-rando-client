9fdd8d5e06ede716bb378ca926b0750eb81f7f3c
Eiko <worst.mirari@gmail.com>
2020-08-18 11:28:42 -0700

    FXGUI wip (mostly done)

---
+++ projects/SeedGen/src/main/scala/FXGUI.scala
@@ -0,0 +1,224 @@
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.stage.DirectoryChooser
+import scalafx.scene.Scene
+import scalafx.scene.effect._
+import scalafx.Includes._
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint.{LinearGradient, Stops}
+import scalafx.scene.text.Text
+import scalafx.scene.layout.BorderPane
+import SeedGenerator._
+import org.json4s._
+import org.json4s.native.Serialization
+import scalafx.beans.property.ObjectProperty
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import java.nio.file.{Paths, Files}
+import java.io.{File, FileWriter}
+package SeedGenerator {
+
+  import java.nio.file.Path
+
+  import scalafx.beans.property.{DoubleProperty, IntegerProperty, StringProperty}
+  import scalafx.scene.control.ScrollPane.ScrollBarPolicy
+  import scalafx.scene.control.{Button, Label, RadioButton, ScrollPane, Tab, TabPane, TextArea, TextInputControl, ToggleButton, ToggleGroup}
+  import scalafx.scene.text.Font
+
+  import scala.io.Source
+  import SeedGenerator.implicits._
+  import javafx.scene.control.{Toggle => Joggle}
+  import scalafx.application.Platform
+  import scalafx.geometry.{Insets, Pos}
+  import scalafx.scene.layout.Priority.Always
+  import scalafx.scene.layout.{Background, BackgroundFill, Border, BorderStroke, BorderStrokeStyle, BorderWidths, CornerRadii, GridPane, VBox}
+
+  import scala.util.Try
+  object FXGUI extends JFXApp {
+    val settingsFile = "SeedGenSettings.json".f.canonPath
+    def settingsFromFile = {
+      implicit val formats: Formats = Serialization.formats(NoTypeHints)
+      settingsFile.read.map(Serialization.read[GenSettings]).getOrElse(GenSettings())
+    }
+    var currentOp: Option[Future[Unit]] = None
+    var outputDirectory = new File(".")
+    val headerFilePath =  ".seedHeader".f.canonPath
+    val folderLabel = new Text {text = s"Output folder: ${outputDirectory.getAbsolutePath}"}
+    val header = new StringProperty(null, "header_text", headerFilePath.read ?? "// Replace this text with a seed header, if desired")
+    val logArea = new TextArea {
+      editable = false
+    }
+    object FXSettingsProvider extends SettingsProvider {
+      def apply() = GenSettings(
+        tps          = teleportersButton.selected(),
+        spoilers     = !raceModeButton.selected(),
+        unsafePaths  = uncheckedPathsButton.selected(),
+        gorlekPaths  = gorlekPathsButton.selected(),
+        glitchPaths  = glitchPathsButton.selected(),
+        questLocs    = questsButton.selected(),
+        outputFolder = outputDirectory.getAbsolutePath,
+        flags        = Flags(forceWispsButton.selected(), forceTreesButton.selected(), forceQuestsButton.selected(), !zoneHintsButton.selected(),
+          !swordSpawnButton.selected(), rainButton.selected(), noKSDoorsButton.selected(), randomSpawnButton.selected()),
+        bonusItems   = bonusItemsButton.selected(),
+        debugInfo    = debugButton.selected(),
+        seirLaunch   = seirLaunchButton.selected()
+      )
+    }
+    object FXLogger extends Logger {
+      override def enabled: Seq[LogLevel] = Seq(INFO, WARN, ERROR) ++ Config().debugInfo ? DEBUG
+      override def write(x: Any): Unit = {
+        //        logArea.setScrollTop(logArea.height())
+        println(x)
+        Platform.runLater(() => logArea.appendText(s"$x\n"))
+      }
+    }
+    Config.settingsProvider = FXSettingsProvider
+    Config.logger  = FXLogger
+
+    val startSet = settingsFromFile
+
+    def onChange(unused: Boolean) = settingsFile.write(FXSettingsProvider().toJson)
+    def toggle(name: String, tooltipText: String, startSelected: => Boolean, listener: => Boolean=>Unit = onChange) =  new ToggleButton(name){
+      selected = startSelected
+      tooltip = tooltipText
+      selected.addListener((_, _, nval) => listener(nval))
+      border <== when (selected) choose
+        new Border(new BorderStroke(stroke = SkyBlue, BorderStrokeStyle.Solid, new CornerRadii(4f), BorderWidths.Default))  otherwise
+        new Border(new BorderStroke(stroke = Black, BorderStrokeStyle.None, new CornerRadii(3f), BorderWidths.Default))
+//      background <== when (selected) choose new Background(Array(new BackgroundFill(LightSkyBlue, new CornerRadii(3f), Insets(1)))) otherwise new Background(Array(new BackgroundFill(LightGrey, new CornerRadii(3f), Insets(1))))
+    }
+
+    val raceModeButton:         ToggleButton = toggle("Race Mode",          "Generate a spoiler-free version of the seed for racing", !startSet.spoilers)
+    val debugButton:            ToggleButton = toggle("Extra Debug Info",   "Output debug info from generation to the activity log (contains spoilers)", startSet.debugInfo)
+    val questsButton:           ToggleButton = toggle("Items on Quests",    "Receive items from quest progress and completion", startSet.questLocs)
+    val zoneHintsButton:        ToggleButton = toggle("Zone Hints",         "Lupo sells the hints", !startSet.flags.noHints)
+    val bonusItemsButton:       ToggleButton = toggle("Bonus Items",        "Enables rando-only bonus pickups, including weapon upgrades", startSet.bonusItems)
+    val teleportersButton:      ToggleButton = toggle("Teleporters",        "Add items to the item pool that unlock teleporters", startSet.tps)
+    val gorlekPathsButton:      ToggleButton = toggle("Gorlek paths",       "Enable Gorlek-difficulty paths", startSet.gorlekPaths)
+    val uncheckedPathsButton:   ToggleButton = toggle("Unsafe paths",       "Enable paths that have not yet been sorted into a difficulty group.", startSet.unsafePaths)
+    val glitchPathsButton:      ToggleButton = toggle("Glitched paths",     "Enable paths that rely on glitches, such as Sentry Jumps", startSet.glitchPaths)
+    val randomSpawnButton:      ToggleButton = toggle("Random spawn",       "Spawn at a randomly-chosen spirit well", startSet.flags.randomSpawn)
+    val swordSpawnButton:       ToggleButton = toggle("Spawn with Sword",   "Start the game with Spirit Edge in your inventory and equipped", !startSet.flags.noSword)
+    val rainButton:             ToggleButton = toggle("Rainy Marsh",        "Start the game in the 'prologue' marsh state, with rain present and Howl enabled", startSet.flags.rain)
+    val seirLaunchButton:       ToggleButton = toggle("Launch on Seir",     "Places launch on Seir", startSet.seirLaunch)
+    val noKSDoorsButton:        ToggleButton = toggle("Remove KS doors",    "Start the game with every keystone door opened", startSet.flags.noKSDoors)
+    val forceWispsButton:       ToggleButton = toggle("Force Wisps",        "Adds requirement: Collect every Wisp", startSet.flags.forceWisps)
+    val forceQuestsButton:      ToggleButton = toggle("Force Quests",       "Adds requirement: Complete every Quest", startSet.flags.forceQuests)
+    val forceTreesButton:       ToggleButton = toggle("Force Trees",        "Adds requirement: Collect all Ancestral Trees", startSet.flags.forceTrees)
+
+    val generateButton = new Button("Generate"){ tooltip = "build a seed with the specified parameters"}
+    val runLastSeedButton = new Button("Run Last Seed")
+    generateButton.onAction = _ => {
+      generateButton.disable = true
+        currentOp = Some(Future {
+          Config.info("Building...")
+/*          if(ui.seedField.text != "") {
+            info(s"Seeded RNG with ${UI.ui.seedField.text}")
+            SeedGenerator.Runner.setSeed(UI.ui.seedField.text.hashCode)
+          }*/
+         val succ = Try {
+            if (SeedGenerator.Runner()) {
+              Config.info(s"Finished generating seed!")
+              true
+            } else {
+              //              lastSeed = None
+              Config.error(s"Failed to generate seed :c")
+              false
+            }
+         }.toOption.contains(true)
+          Platform.runLater(() => {
+            runLastSeedButton.disable = !succ
+            generateButton.disable = false
+            currentOp = None
+          })
+        })
+      }
+
+    val textArea: TextArea = new TextArea() {
+      text <==> header
+    }
+
+    header.addListener((o, oldV, newV) => {
+      println(newV.count(a => Seq('\n', '\r').contains(a)))
+      headerFilePath.write(newV)
+    })
+    stage = new PrimaryStage {
+      title = "Wotw Rando Seed Generator"
+      width = 800
+      height = 600
+      val selector = new DirectoryChooser {
+        initialDirectory = outputDirectory
+      }
+
+      val headerInputField = new TextArea() {
+        font = Font("Monospaced")
+      }
+      val tabPane: TabPane = getTabPane
+
+
+      scene = new Scene {
+        fill = LightGray
+        content = tabPane
+      }
+//      textArea.prefRowCount <== header().count(a => Seq('\n', '\r').contains(a)) + 6
+      textArea.prefWidth <== tabPane.prefWidth - 40
+      tabPane.prefWidth <== scene.value.widthProperty()
+      tabPane.prefHeight <== scene().heightProperty()
+
+
+    }
+
+    private def getTabPane = {
+      new TabPane {
+        border = new Border(new BorderStroke(stroke = Black, BorderStrokeStyle.Solid, new CornerRadii(3f), BorderWidths.Default))
+        padding = Insets(15)
+        tabs = Seq(
+          getMainTab,
+          getHeaderTab
+        )
+      }
+    }
+    private def getOptions: GridPane = {
+      val gp = new GridPane() {
+
+      }
+
+      gp.addRow(0, new Label("Logic Groups: "), gorlekPathsButton, glitchPathsButton, uncheckedPathsButton)
+      gp.addRow(1, new Label("Goal Modes: "), forceTreesButton, forceWispsButton, forceQuestsButton)
+      gp.addRow(2, generateButton, debugButton,
+        new Button("Clear Log"){onAction = _ => logArea.setText("")},
+      )
+      gp
+    }
+    private def getMainTab = new Tab {
+      closable = false
+      id = "main"
+      text = "main"
+      tooltip = "Main seedgen tab"
+      content = new BorderPane {
+        top = getOptions
+        center = logArea
+      }
+    }
+
+    private def getHeaderTab = new Tab {
+        closable = false
+        id = "header"
+        text = "header"
+        tooltip = "for customizing seeds"
+        content = new VBox() {
+          children = Seq(
+            new Text("Any text added below will be placed at the top of your seed file.\nYou can use this to preplace items") { alignmentInParent = Pos.Center},
+            textArea
+          )
+        }
+    }
+
+    def show() = {
+      main(Array.empty[String])
+    }
+  }
+
+}
