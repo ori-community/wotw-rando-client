6b1a0f16ddc7f2a7a5d6bb0708cfde768da00d8b
badulf96@gmail.com <badulf96@gmail.com>
2020-09-01 21:47:51 +0100

    Added protobuf compiler for CSharp to ext Added automatic code gen for RandoMainDLL when cmake is run. Added initial setup for websocket client.

---
+++ projects/RandoMainDLL/WebSocketClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using Google.Protobuf;
+using Network;
+using WebSocketSharp;
+
+namespace RandoMainDLL
+{
+  public class WebSocketClient {
+    private const string ServerAddress = "ws://somethingsomething";
+
+    private static WebSocket socket;
+
+    public void Connect() {
+      socket = new WebSocket(ServerAddress);
+      socket.OnMessage += HandleMessage;
+      socket.Connect();
+      //socket.Send("test");
+    }
+
+    public void SendUpdate(int group, int state, float value) {
+      Packet packet = new Packet {
+        Id = 3,
+        Packet_ = new UberStateUpdateMessage {
+          State = new UberId {
+            Group = group,
+            State = state
+          },
+          Value = value
+        }.ToByteString()
+      };
+
+      socket.Send(packet.ToByteArray());
+    }
+
+    public void HandleMessage(object sender, MessageEventArgs args) {
+      var packet = Packet.Parser.ParseFrom(args.RawData);
+      switch (packet.Id) {
+        case 5:
+          var init = InitBingoMessage.Parser.ParseFrom(packet.Packet_);
+          break;
+        case 3:
+          var update = UberStateUpdateMessage.Parser.ParseFrom(packet.Packet_);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
