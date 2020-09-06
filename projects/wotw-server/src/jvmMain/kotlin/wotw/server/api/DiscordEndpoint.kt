cff89d29b82ffb302839eb1ee6c7ae4cce914122
Eiko <worst.mirari@gmail.com>
2020-09-06 16:21:43 -0700

    wip server stuff

--- projects/wotw-server/src/jvmMain/kotlin/wotw/server/api/DiscordEndpoint.kt
+++ projects/wotw-server/src/jvmMain/kotlin/wotw/server/api/DiscordEndpoint.kt
@@ -12,11 +12,13 @@ import com.gitlab.kordlib.*
 import com.gitlab.kordlib.core.Kord
 import com.gitlab.kordlib.core.builder.kord.KordBuilder
 import wotw.server.main.WotwBackendServer
+import wotw.server.util.logger
 
 class DiscordEndpoint(server: WotwBackendServer) : Endpoint(server) {
-
+    val logger = logger()
     override fun Routing.initRouting() {
         get("login"){
+            logger.info("login called")
             call.respondRedirect("https://discord.com/api/oauth2/authorize?client_id=751523174767919195&redirect_uri=http%3A%2F%2F127.0.0.1%2Foauth%2Fredir&response_type=code&scope=identify", false)
         }
         get("oauth/redir"){
