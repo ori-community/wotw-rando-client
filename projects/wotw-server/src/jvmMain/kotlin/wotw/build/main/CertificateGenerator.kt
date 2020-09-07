a7c55badb626a7bc906c447168958e1db128b7cb
Xemsys <xemsys@gmail.com>
2020-09-07 21:41:43 +0200

    Maybe commit all of it

---
+++ projects/wotw-server/src/jvmMain/kotlin/wotw/build/main/CertificateGenerator.kt
@@ -0,0 +1,22 @@
+package wotw.build.main
+
+import io.ktor.network.tls.certificates.generateCertificate
+import java.io.File
+
+//For local testing purposes
+class CertificateGenerator {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val jkspw = System.getenv("JKSPW") ?: throw Exception("JKSPW not set!")
+            val keypw = System.getenv("KEYPW") ?: throw Exception("KEYPW not set!")
+
+            val jksFile = File("build/temporary.jks").apply {
+                parentFile.mkdirs()
+            }
+            if (!jksFile.exists()) {
+                generateCertificate(jksFile, keyAlias = "wotwserverkey", jksPassword = jkspw, keyPassword = keypw)
+            }
+        }
+    }
+}
