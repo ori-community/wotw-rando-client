823ec2c10b82aaaa85abe9bc14eed50ebc2e61ea
Eiko <worst.mirari@gmail.com>
2020-09-14 15:16:05 -0700

    look it's fine

--- projects/wotw-server/src/commonMain/kotlin/wotw/io/messages/protobuf/GameSyncMessages.kt
+++ projects/wotw-server/src/commonMain/kotlin/wotw/io/messages/protobuf/GameSyncMessages.kt
@@ -22,7 +22,7 @@ data class InitBingoMessage(
 
 @Serializable
 data class PrintTextMessage(
-    @ProtoNumber(1) val Frames: Int = 200,
+    @ProtoNumber(1) val frames: Int = 200,
     @ProtoNumber(2) val y_pos: Float = 3f,
     @ProtoNumber(3) val text: String
 )
