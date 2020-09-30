31b150d39e6a2ba107737f0153e7e30de5f69964
Eiko <worst.mirari@gmail.com>
2020-09-30 15:28:53 -0700

    everything about this has been infuriating

--- projects/RandoMainDLL/WebSocketClient.cs
+++ projects/RandoMainDLL/WebSocketClient.cs
@@ -22,32 +22,24 @@ namespace RandoMainDLL {
     public int FramesSinceLastCheck = 0;
     private string ServerAddress => $"ws{S}://{Domain}/api/game_sync/";
 
+    private Thread updateThread;
+    private Thread connectThread;
+
+    public static int FramesTillReconnectAttempt = 420;
+
     private WebSocket socket;
     public bool IsConnected { get { return socket != null && socket.IsConnected; } }
-    public bool Connecting = false;
+    public bool Connecting { get => connectThread?.IsAlive ?? false; }
     public void Connect() {
       if (DiscordController.Disabled ) return;
-      if(updateThread == null) {
-        updateThread = new Thread(() => {
-          while(true) {
-            try {
-              var packet = SendQueue.Take();
-              socket.Send(packet.ToByteArray());
-            }
-            catch (Exception e) {
-              Randomizer.Warn("UpdateThread", $"caught error {e}");
-            }
-          }
-        });
-        updateThread.Start();
-      }
-      if(Connecting) {
+      setupUpdateThread();
+
+      if (Connecting) {
         Randomizer.Log("Skipping connection request as one is in-progress", false, "DEBUG");
         FramesTillReconnectAttempt = 120;
         return;
       }
-      new Thread(() => {
-        Connecting = true;
+      connectThread = new Thread(() => {
         //      PlayerId = player;
         if (socket != null) {
           Disconnect();
@@ -58,8 +50,7 @@ namespace RandoMainDLL {
           if (user == null) {
             Randomizer.Log("Have no user ID; reattempting discord auth", false, "WARN");
             DiscordController.Initialize();
-            Connecting = false;
-            FramesTillReconnectAttempt = 30;
+            FramesTillReconnectAttempt = 60;
             return;
           }
           var client = new WebClient();
@@ -73,65 +64,82 @@ namespace RandoMainDLL {
             SessionId = rawCookie.Split(';')[0].Split('=')[1];
           } catch (Exception e2) {
             Randomizer.Error($"Failed to parse cookie {rawCookie} (response headers: {client.ResponseHeaders})", e2);
-            Connecting = false;
             FramesTillReconnectAttempt = 120;
             return;
           }
         }
         catch (Exception e) { 
           Randomizer.Error($"Connect (UploadString, user had id {user?.Id}", e);
-          Connecting = false;
           FramesTillReconnectAttempt = 120;
           return;
         }
 
 
-        socket = new WebSocket(ServerAddress, null);
-        socket.CookieCollection.Add(new WebSocketSharp.Net.Cookie("sessionid", SessionId, "/", Domain));
-        socket.Log.Level = LogLevel.Info;
-        socket.Log.Output = (logdata, output) => {
-          Randomizer.Log($"Websocket says: {logdata.Message}", false, $"{logdata.Level}");
-        };
-        socket.OnError += (sender, e) => {
-          Randomizer.Error("WebSocket", $"{e} {e?.Exception}", false);
-          FramesTillReconnectAttempt = 300;
-          Connecting = false;
-        };
-        socket.OnClose += (sender, e) => {
-          if(!ExpectingDisconnect)
-            Randomizer.Log("Disconnected! Retrying in 5s");
-        };
-        socket.OnMessage += HandleMessage;
-        socket.OnOpen += (sender, args) => {
-          Connecting = false;
-          Randomizer.Log($"Connected to server", false);
-          UberStateController.QueueSyncedStateUpdate();
-        };
-        Randomizer.Log($"Attempting to connect to ${Domain}", false);
         try {
+          socket = new WebSocket(ServerAddress, null);
+          socket.CookieCollection.Add(new WebSocketSharp.Net.Cookie("sessionid", SessionId, "/", Domain));
+          socket.Log.Level = LogLevel.Info;
+          socket.Log.Output = (logdata, output) => {
+            try {
+              if (logdata != null)
+                Randomizer.Log($"Websocket says: {logdata.Message}", false, $"{logdata.Level}");
+              else
+                Randomizer.Log($"Websocket output: {output}", false);
+            } catch (Exception e) {
+              Randomizer.Error("Websocket log", e, false);
+            }
+          };
+          socket.OnError += (sender, e) => {
+            Randomizer.Error("WebSocket", $"{e} {e?.Exception}", false);
+            FramesTillReconnectAttempt = 300;
+          };
+          socket.OnClose += (sender, e) => {
+            if(!ExpectingDisconnect)
+              Randomizer.Log("Disconnected! Retrying in 5s");
+          };
+          socket.OnMessage += HandleMessage;
+          socket.OnOpen += (sender, args) => {
+            Randomizer.Log($"Connected to server", false);
+            UberStateController.QueueSyncedStateUpdate();
+          };
+          Randomizer.Log($"Attempting to connect to {Domain}", false);
           socket.Connect();
-        } catch(Exception e) {
-          Connecting = false;
+        } catch (Exception e) {
           FramesTillReconnectAttempt = 120;
           Randomizer.Error("Connect (socket.)", e, false); 
         }
 
-      }).Start();
+      });
+      connectThread.Start();
     }
 
-    public Thread updateThread;
-
-    public static int FramesTillReconnectAttempt = 420;
     public void Update() {
-      if (!DiscordController.Disabled && !IsConnected) {
+      if (!DiscordController.Disabled && !IsConnected && !Connecting) {
         if (FramesTillReconnectAttempt-- <= 0) {
-          FramesTillReconnectAttempt = 0;
+          FramesTillReconnectAttempt = 600;
           Randomizer.Log("Want connection but currently have none, attempting reconnect", false);
           Connect();
         }
       }
     }
 
+    private void setupUpdateThread() {
+      if (updateThread == null) {
+        updateThread = new Thread(() => {
+          while (true) {
+            if (IsConnected)
+              try {
+                var packet = SendQueue.Take();
+                socket.Send(packet.ToByteArray());
+              }
+              catch (Exception e) {
+                Randomizer.Warn("UpdateThread", $"caught error {e}");
+              }
+          }
+        });
+        updateThread.Start();
+      }
+    }
     public void Disconnect() {
       if (socket == null) {
         return;
