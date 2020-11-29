bc01a70cd3696ac3b0fd08c953a2077d15a7e2f4
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2020-11-29 02:36:42 +0100

    MarshTP is boring

--- projects/SeedGen/src/main/scala/Items.scala
+++ projects/SeedGen/src/main/scala/Items.scala
@@ -183,7 +183,7 @@ package SeedGenerator {
 
   object Teleporter {
     val itemType: Int = 5
-    val costs: Map[Int, Double] = Map(3 -> 14, 16 -> 14, 17->10)
+    val costs: Map[Int, Double] = Map(3 -> 14, 16 -> 30, 17->10)
     val areaFileNames = Map("BurrowsTP" -> 0, "DenTP" -> 1, "EastPoolsTP" -> 2, "WellspringTP" -> 3, "ReachTP" -> 4, "HollowTP" -> 5, "DepthsTP" -> 6, "WestWoodsTP" -> 7, "EastWoodsTP" -> 8, "WestWastesTP" -> 9, "EastWastesTP" -> 10, "OuterRuinsTP" -> 11, "WillowTP" -> 12, "Willow'sEndTP" -> 12, "WestPoolsTP" -> 13, "InnerRuinsTP" -> 14, "MarshTP"->16, "GladesTP" -> 17)
     val names: Map[Int, String] = Map(
       0 -> "Burrows",
