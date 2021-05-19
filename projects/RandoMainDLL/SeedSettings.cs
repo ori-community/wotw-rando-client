6c55b8156b37a9b1b1599e7221dd7f5711fbc002
Eiko <worst.mirari@gmail.com>
2021-05-19 12:00:58 -0700

    remove unused settings fields, error on settings parse fail

--- projects/RandoMainDLL/SeedSettings.cs
+++ projects/RandoMainDLL/SeedSettings.cs
@@ -7,8 +7,6 @@ namespace RandoMainDLL {
   public class SeedGenSettings {
     [JsonIgnore]
     private static Version rustGenVersion = new Version("0.13.0");
-    [JsonProperty("tps")]
-    public Boolean Teleporters { get; set; }
     [JsonProperty("spoilers")]
     public Boolean Spoilers { get; set; }
     [JsonIgnore]
@@ -23,18 +21,6 @@ namespace RandoMainDLL {
     }
     [JsonIgnore]
     public bool LegacySeedgen => 0 > Version.CompareTo(rustGenVersion);
-    [JsonProperty("unsafePaths")]
-    public Boolean UnsafePaths { get; set; }
-    [JsonProperty("gorlekPaths")]
-    public Boolean GorlekPaths { get; set; }
-    [JsonProperty("glitchPaths")]
-    public Boolean GlitchPaths { get; set; }
-    [JsonProperty("questLocs")]
-    public Boolean ItemsOnQuests { get; set; }
-    [JsonProperty("bonusItems")]
-    public Boolean BonusItems { get; set; }
-    [JsonProperty("randomSpawn")]
-    public Boolean RandomSpawn { get; set; }
     [JsonProperty("webConn")]
     public Boolean NetcodeEnabled { get; set; }
   }
