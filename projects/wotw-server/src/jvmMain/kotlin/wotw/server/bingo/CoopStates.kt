8557b5e06c0fad93fda996637a744dae6bb1d048
Eiko <worst.mirari@gmail.com>
2021-05-18 21:50:23 -0700

    WHOOPS

--- projects/wotw-server/src/jvmMain/kotlin/wotw/server/bingo/CoopStates.kt
+++ projects/wotw-server/src/jvmMain/kotlin/wotw/server/bingo/CoopStates.kt
@@ -661,6 +661,7 @@ val coopAggregation by lazy {
             sync(questIds.values),
             sync(pickupIds.values),
             sync(unsortedCoop),
+            sync(multiStates()),
             sync(37858, 8487).on(threshold = 5f), //Wellspring fight room
             sync(5377, 53480).on(threshold = 4f), // pools fight room 2
             sync(9593, 25130).on(threshold = 3f) // double jump lizard fight
