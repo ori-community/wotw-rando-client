7d152dc84d50cd4b97279873b079fdc82b9e0b27
Timo Schwarzer <timo@schwarzer.dev>
2021-07-20 22:59:10 +0200

    server be gone

--- projects/wotw-server/src/jsMain/kotlin/wotw/web/bindings/WrapperData.kt
+++
@@ -1,5 +0,0 @@
-package wotw.web.bindings
-
-import wotw.web.bindings.IRSelectOption
-
-data class RSelectOption(override val value: Any, override val label: String) : IRSelectOption
--- projects/wotw-server/src/jvmMain/kotlin/wotw/server/exception/ApiExceptions.kt
+++
@@ -1,5 +0,0 @@
-package wotw.server.exception
-
-class AlreadyExistsException(message: String? = null) : Exception(message)
-class UnauthorizedException : Exception()
-class ServerConfigurationException(message: String? = null): Exception(message)
