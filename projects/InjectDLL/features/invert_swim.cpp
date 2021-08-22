f2536a20208d53aae34e87da2b426006d496e31e
MunWolf <badulf96@gmail.com>
2021-08-22 05:37:38 +0100

    Added different velocity upgrades.

--- projects/InjectDLL/features/invert_swim.cpp
+++ projects/InjectDLL/features/invert_swim.cpp
@@ -4,16 +4,22 @@
 #include <csharp_bridge.h>
 
 #include <Il2CppModLoader/interception_macros.h>
+#include <uber_states\uber_state_manager.h>
 
 namespace
 {
+    constexpr float DEFAULT_SWIM_SPEED = 6.f;
+    constexpr float DEFAULT_BOOSTED_SWIM_SPEED = 9.6f;
+
+    constexpr int SWIM_SPEED_MULTIPLIER = 85;
+
     STATIC_IL2CPP_BINDING(UnityEngine, AnimationCurve, app::AnimationCurve*, EaseInOut, (float timeStart, float valueStart, float timeEnd, float valueEnd));
 
     void set_swim_params(float normal, float boost) {
         auto swim = get_sein()->fields.Abilities->fields.SwimmingWrapper;
         if (swim->fields.HasState)
         {
-            swim->fields.State->fields.SwimSpeed = normal;
+            swim->fields.State->fields.SwimSpeed = normal * uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, SWIM_SPEED_MULTIPLIER);
             swim->fields.State->fields.SwimSpeedBoostCurve = AnimationCurve::EaseInOut(0.05, 1.f, 0.2, boost / normal);
         }
     }
@@ -42,7 +48,7 @@ namespace
 void invert_swim() {
     // Default boosted speed is 9.59... so we use 9.6
     if (csharp_bridge::invert_swim())
-        set_swim_params(9.6f, 6.f);
+        set_swim_params(DEFAULT_BOOSTED_SWIM_SPEED, DEFAULT_SWIM_SPEED);
     else
-        set_swim_params(6.f, 9.6f);
+        set_swim_params(DEFAULT_SWIM_SPEED, DEFAULT_BOOSTED_SWIM_SPEED);
 }
---
+++ projects/InjectDLL/pickups/bonus/velocity.cpp
@@ -0,0 +1,98 @@
+#include <dll_main.h>
+#include <csharp_bridge.h>
+#include <features\invert_swim.h>
+
+#include <Common/ext.h>
+#include <Il2CppModLoader/il2cpp_helpers.h>
+#include <Il2CppModLoader/common.h>
+#include <Il2CppModLoader/interception_macros.h>
+
+#include <uber_states\uber_state_manager.h>
+
+namespace
+{
+    constexpr int CHARGE_SPEED_MULTIPLIER_ID = 80;
+    constexpr int DASH_DISTANCE_MULTIPLIER_ID = 81;
+    constexpr int BASH_SPEED_MULTIPLIER = 82;
+    constexpr int BURROW_SPEED_MULTIPLIER = 83;
+    constexpr int BURROW_DASH_SPEED_MULTIPLIER = 84;
+    constexpr int SWIM_DASH_SPEED_MULTIPLIER = 86;
+
+    float initial_jump_speed;
+
+    IL2CPP_INTERCEPT(, SeinChargeJump, void, OnAwake, (app::SeinChargeJump* this_ptr)) {
+        SeinChargeJump::OnAwake(this_ptr);
+        initial_jump_speed = this_ptr->fields.JumpSpeed;
+    }
+
+    IL2CPP_INTERCEPT(, SeinChargeJump, void, EnterMove, (app::SeinChargeJump* this_ptr)) {
+        this_ptr->fields.JumpSpeed = initial_jump_speed * uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, CHARGE_SPEED_MULTIPLIER_ID);
+        SeinChargeJump::EnterMove(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SeinDashNew, void, OnProcessRootMotion, (app::SeinDashNew * this_ptr, app::Vector3 root_velocity)) {
+        float modifier = uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, DASH_DISTANCE_MULTIPLIER_ID);
+        root_velocity.x *= modifier;
+        root_velocity.y *= modifier;
+        root_velocity.z *= modifier;
+        SeinDashNew::OnProcessRootMotion(this_ptr, root_velocity);
+    }
+
+    float initial_bash_speed;
+    float initial_bash_force;
+    float initial_bash_enemy_force;
+
+    IL2CPP_INTERCEPT(, SeinBashAttack, void, Start, (app::SeinBashAttack* this_ptr)) {
+        initial_bash_speed = this_ptr->fields.BashVelocity;
+        initial_bash_enemy_force = this_ptr->fields.EnemyThrowForce;
+        SeinBashAttack::Start(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SeinBashAttack, void, BeginBash, (app::SeinBashAttack* this_ptr)) {
+        float modifier = uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, BASH_SPEED_MULTIPLIER);
+        this_ptr->fields.BashVelocity = initial_bash_speed * modifier;
+        this_ptr->fields.EnemyThrowForce = initial_bash_enemy_force * modifier;
+        SeinBashAttack::BeginBash(this_ptr);
+    }
+
+    float initial_burrow_speed;
+    float initial_burrow_dash;
+
+    IL2CPP_INTERCEPT(, SeinDigging, void, OnAwake, (app::SeinDigging* this_ptr)) {
+        initial_burrow_speed = this_ptr->fields.DigSpeed;
+        initial_burrow_dash = this_ptr->fields.DashSpeed;
+        SeinDigging::OnAwake(this_ptr);
+    }
+    
+    IL2CPP_INTERCEPT(, SeinDigging, void, UpdateDiggingState, (app::SeinDigging* this_ptr)) {
+        float modifier = uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, BURROW_SPEED_MULTIPLIER);
+        this_ptr->fields.DigSpeed = initial_burrow_speed * modifier;
+        SeinDigging::UpdateDiggingState(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SeinDigging, void, StartDashing, (app::SeinDigging* this_ptr)) {
+        float modifier = uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, BURROW_DASH_SPEED_MULTIPLIER);
+        this_ptr->fields.DashSpeed = initial_burrow_dash * modifier;
+        SeinDigging::StartDashing(this_ptr);
+    }
+
+    float initial_swim_speed;
+    float initial_swim_dash;
+
+    IL2CPP_INTERCEPT(, SeinSwimming, void, OnAwake, (app::SeinSwimming* this_ptr)) {
+        initial_swim_speed = this_ptr->fields.SwimSpeed;
+        initial_swim_dash = this_ptr->fields.DashMaxSpeed;
+        SeinSwimming::OnAwake(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SeinSwimming, void, UpdateCharacterState, (app::SeinSwimming* this_ptr)) {
+        invert_swim();
+        SeinSwimming::UpdateCharacterState(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(, SeinSwimming, void, StartDashing, (app::SeinSwimming* this_ptr)) {
+        float modifier = uber_states::get_uber_state_value(uber_states::constants::RANDO_UPGRADE_GROUP_ID, SWIM_DASH_SPEED_MULTIPLIER);
+        this_ptr->fields.DashMaxSpeed = initial_swim_dash * modifier;
+        SeinSwimming::StartDashing(this_ptr);
+    }
+}
