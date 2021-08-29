966f82746363b99d90ee540b28a5d64a0e561368
MunWolf <badulf96@gmail.com>
2021-08-29 10:52:44 +0100

    Added controller rebinding functionality, modify controller_bindings.cfg in the rando directory to rebind your controller.

--- projects/Common/ext.h
+++ projects/Common/ext.h
@@ -11,23 +11,26 @@ const char* format(const char* str, ...);
 const char* format(const char* str, va_list ls);
 
 // trim from start (in place)
-static inline void ltrim(std::string& s) {
+static inline std::string& ltrim(std::string& s) {
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
         return !std::isspace(ch);
     }));
+    return s;
 }
 
 // trim from end (in place)
-static inline void rtrim(std::string& s) {
+static inline std::string& rtrim(std::string& s) {
     s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
         return !std::isspace(ch);
     }).base(), s.end());
+    return s;
 }
 
 // trim from both ends (in place)
-static inline void trim(std::string& s) {
+static inline std::string& trim(std::string& s) {
     ltrim(s);
     rtrim(s);
+    return s;
 }
 
 // trim from start (copying)
