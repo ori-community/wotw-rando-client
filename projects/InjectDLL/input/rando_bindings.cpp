c04e3a6fdc834b0252462a03426d9f5dfb17e964
MunWolf <badulf96@gmail.com>
2021-09-08 11:43:06 +0100

    Add keyboard rebinding file. General input improvements.

--- projects/InjectDLL/input/rando_bindings.cpp
+++ projects/InjectDLL/input/rando_bindings.cpp
@@ -2,32 +2,171 @@
 #include <input/controller_bindings.h>
 #include <macros.h>
 
+#include <Common/ext.h>
+
+#include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception_macros.h>
 #include <Il2CppModLoader/il2cpp_helpers.h>
 
+#include <fstream>
 #include <unordered_map>
 #include <unordered_set>
 
+#include <magic_enum/include/magic_enum.hpp>
+
+using namespace modloader;
+
 namespace input
 {
-    struct KeyboardMouseInput
+    namespace
     {
-        std::vector<app::KeyCode__Enum> codes;
-        std::vector<int> mouse_buttons;
-    };
+        const std::string KEYBOARD_REBIND_FILE = "keyboard_bindings.rkbn";
 
-    struct ControlInfo
-    {
-        std::vector<rando_input_callback> on_pressed_actions;
-        std::vector<rando_input_callback> on_release_actions;
-        std::vector<KeyboardMouseInput> kbm_bindings;
-        bool simulated_state = false;
+        struct KeyboardMouseInput
+        {
+            std::vector<app::KeyCode__Enum> codes;
+            std::vector<int> mouse_buttons;
+        };
 
-        bool is_pressed = false;
-        bool is_just_pressed = false;
-    };
+        struct ControlInfo
+        {
+            std::vector<rando_input_callback> on_pressed_actions;
+            std::vector<rando_input_callback> on_release_actions;
+            std::vector<KeyboardMouseInput> kbm_bindings;
+            bool simulated_state = false;
 
-    std::unordered_map<Action, ControlInfo> bindings;
+            bool is_pressed = false;
+            bool is_just_pressed = false;
+        };
+
+        std::unordered_map<Action, ControlInfo> bindings;
+
+        void add_keyboard_binding(Action action, KeyboardMouseInput input)
+        {
+            if (action < Action::OpenRandoWheel)
+                return;
+
+            if (input.codes.empty() && input.mouse_buttons.empty())
+                return;
+
+            bindings[action].kbm_bindings.push_back(input);
+        }
+
+        KeyboardMouseInput parse_input(std::string const& line)
+        {
+            KeyboardMouseInput input;
+            std::vector<std::string> elements;
+            split_str(line, elements, ',');
+            for (auto const& element : elements)
+            {
+                int value = std::stoi(element);
+                if (value < 0)
+                    input.mouse_buttons.push_back(-value);
+                else
+                    input.codes.push_back(static_cast<app::KeyCode__Enum>(value));
+            }
+        }
+
+        void read_bindings()
+        {
+            bindings.clear();
+            std::string line;
+            std::ifstream file(base_path + KEYBOARD_REBIND_FILE);
+            while (std::getline(file, line))
+            {
+                line = trim(line.substr(0, line.find('#')));
+                if (!line.empty())
+                {
+                    auto index = line.find('=');
+                    if (index != std::string::npos)
+                    {
+                        auto action = magic_enum::enum_cast<Action>(trim(line.substr(0, index)));
+                        if (!action.has_value())
+                        {
+                            modloader::warn("bindings", format("Invalid action in binding file (%s), skipping.", line));
+                            continue;
+                        }
+
+                        try
+                        {
+                            add_keyboard_binding(action.value(), parse_input(line));
+                        }
+                        catch (std::exception e) { modloader::warn("bindings", format("Invalid keys in binding file (%s), skipping. [%s]", line, e.what())); }
+                    }
+                    else
+                        modloader::warn("bindings", format("Invalid line in binding file (%s), skipping.", line));
+                }
+            }
+        }
+
+        IL2CPP_INTERCEPT(, PlayerInput, void, ClearControls, (app::PlayerInput* this_ptr)) {
+            PlayerInput::ClearControls(this_ptr);
+            for (auto& binding : bindings)
+                binding.second.kbm_bindings.clear();
+        }
+
+        IL2CPP_INTERCEPT(, PlayerInput, void, AddKeyboardControls, (app::PlayerInput* this_ptr)) {
+            PlayerInput::AddKeyboardControls(this_ptr);
+            read_bindings();
+        }
+
+        bool is_pressed(Action action)
+        {
+            if (action < Action::OpenRandoWheel)
+                return false;
+
+            return bindings[action].is_pressed;
+        }
+
+        STATIC_IL2CPP_BINDING(UnityEngine, Input, bool, GetKeyInt, (app::KeyCode__Enum key));
+        STATIC_IL2CPP_BINDING(UnityEngine, Input, bool, GetMouseButton, (int button));
+        bool is_pressed(KeyboardMouseInput const& input)
+        {
+            for (auto code : input.codes)
+                if (!Input::GetKeyInt(code))
+                    return false;
+
+            for (auto button : input.mouse_buttons)
+                if (!Input::GetMouseButton(button))
+                    return false;
+        
+            return true;
+        }
+
+        IL2CPP_INTERCEPT(, PlayerInput, void, RefreshControls, (app::PlayerInput* this_ptr)) {
+            PlayerInput::RefreshControls(this_ptr);
+            refresh_controller_controls();
+
+            for (auto& binding : bindings)
+            {
+                auto pressed = is_controller_pressed(binding.first);
+                if (!pressed)
+                {
+                    for (auto const& input : binding.second.kbm_bindings)
+                    {
+                        if (is_pressed(input))
+                        {
+                            pressed = true;
+                            break;
+                        }
+                    }
+                }
+
+                pressed |= binding.second.simulated_state;
+
+                auto is_just_released = !pressed && binding.second.is_pressed;
+                binding.second.is_just_pressed = pressed && !binding.second.is_pressed;
+                binding.second.is_pressed = pressed;
+                if (binding.second.is_just_pressed)
+                    for (auto action : binding.second.on_pressed_actions)
+                        action();
+
+                if (is_just_released)
+                    for (auto action : binding.second.on_release_actions)
+                        action();
+            }
+        }
+    }
 
     void add_on_pressed_callback(Action action, rando_input_callback callback)
     {
@@ -43,84 +182,6 @@ namespace input
             return;
 
         bindings[action].on_release_actions.push_back(callback);
-    }
-
-    void add_keyboard_binding(Action action, KeyboardMouseInput input)
-    {
-        if (action < Action::OpenRandoWheel)
-            return;
-
-        bindings[action].kbm_bindings.push_back(input);
-    }
-
-    IL2CPP_INTERCEPT(, PlayerInput, void, ClearControls, (app::PlayerInput* this_ptr)) {
-        PlayerInput::ClearControls(this_ptr);
-        for (auto& binding : bindings)
-            binding.second.kbm_bindings.clear();
-    }
-
-    IL2CPP_INTERCEPT(, PlayerInput, void, AddKeyboardControls, (app::PlayerInput* this_ptr)) {
-        PlayerInput::AddKeyboardControls(this_ptr);
-
-        // TODO: Read a keyboard bindings file and add bindings instead of the hardcoded ones.
-        add_keyboard_binding(Action::OpenRandoWheel, KeyboardMouseInput{ { app::KeyCode__Enum_V }, {} });
-    }
-
-    bool is_pressed(Action action)
-    {
-        if (action < Action::OpenRandoWheel)
-            return false;
-
-        return bindings[action].is_pressed;
-    }
-
-    STATIC_IL2CPP_BINDING(UnityEngine, Input, bool, GetKeyInt, (app::KeyCode__Enum key));
-    STATIC_IL2CPP_BINDING(UnityEngine, Input, bool, GetMouseButton, (int button));
-    bool is_pressed(KeyboardMouseInput const& input)
-    {
-        for (auto code : input.codes)
-            if (!Input::GetKeyInt(code))
-                return false;
-
-        for (auto button : input.mouse_buttons)
-            if (!Input::GetMouseButton(button))
-                return false;
-        
-        return true;
-    }
-
-    IL2CPP_INTERCEPT(, PlayerInput, void, RefreshControls, (app::PlayerInput* this_ptr)) {
-        PlayerInput::RefreshControls(this_ptr);
-        refresh_controller_controls();
-
-        for (auto& binding : bindings)
-        {
-            auto pressed = is_controller_pressed(binding.first);
-            if (!pressed)
-            {
-                for (auto const& input : binding.second.kbm_bindings)
-                {
-                    if (is_pressed(input))
-                    {
-                        pressed = true;
-                        break;
-                    }
-                }
-            }
-
-            pressed |= binding.second.simulated_state;
-
-            auto is_just_released = !pressed && binding.second.is_pressed;
-            binding.second.is_just_pressed = pressed && !binding.second.is_pressed;
-            binding.second.is_pressed = pressed;
-            if (binding.second.is_just_pressed)
-                for (auto action : binding.second.on_pressed_actions)
-                    action();
-
-            if (is_just_released)
-                for (auto action : binding.second.on_release_actions)
-                    action();
-        }
     }
 
     void simulate_action(Action action, bool value)
