eee6d183c9f1947fb3e3a22f3cded1f758ee7a5c
MunWolf <badulf96@gmail.com>
2021-09-16 23:58:23 +0100

    Multiplayer things (untested)
    Added handling of player messages and position updates.
    Added UDP client.

---
+++ projects/RandoMainDLL/Multiplayer.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Network;
+using RandoMainDLL.Memory;
+
+namespace RandoMainDLL {
+
+  public static class Multiplayer {
+    public static BlockingCollection<Packet> Queue = new BlockingCollection<Packet>();
+
+    public static string Id { get; private set; }
+    public static string Name { get; private set; }
+
+    private static HashSet<string> currentPlayers = new HashSet<string>();
+    private static MultiverseInfoMessage lastMultiverseInfo = null;
+
+    public static void SetCurrentUser(UserInfo user) {
+      Id = user.Id;
+      Name = user.Name;
+      if (lastMultiverseInfo != null)
+        UpdateMultivere(lastMultiverseInfo);
+    }
+
+    public static void UpdateMultivere(MultiverseInfoMessage multiverse) {
+      lastMultiverseInfo = multiverse;
+      var universe = multiverse.Universes.First(u => u.Worlds.Any(w => w.Members.Any(m => m.Id == Id)));
+      if (universe != null) {
+        var players = new Dictionary<string, string>();
+        foreach (var world in universe.Worlds)
+          foreach (var member in world.Members)
+            if (member.Id != Id)
+              players.Add(member.Id, member.Name);
+
+        var toAdd = players.Keys.Except(currentPlayers);
+        var toRemove = currentPlayers.Except(players.Keys);
+
+        foreach (var player in toRemove)
+          InterOpMultiplayer.remove_player(player);
+
+        foreach (var player in toAdd)
+          InterOpMultiplayer.add_player(player, players[player]);
+
+        currentPlayers = toAdd.ToHashSet();
+      }
+    }
+
+    public static void UpdatePlayerPosition(string id, float x, float y) {
+      if (currentPlayers.Any(p => p == id)) {
+        InterOpMultiplayer.update_player_position(id, x, y);
+      }
+      else {
+        Randomizer.Log($"Got player position from unknown player {id}: {x}, {y}");
+      }
+    }
+
+    public static void Update() {
+      while (Queue.TryTake(out var packet)) {
+        switch (packet.Id) {
+          case 8:
+            var multiverse = MultiverseInfoMessage.Parser.ParseFrom(packet.Packet_);
+            UpdateMultivere(multiverse);
+            break;
+          case 11:
+            var position = UpdatePlayerPositionMessage.Parser.ParseFrom(packet.Packet_);
+            UpdatePlayerPosition(position.PlayerId, position.X, position.Y);
+            break;
+          case 12:
+            var authenticated = AuthenticatedMessage.Parser.ParseFrom(packet.Packet_);
+            SetCurrentUser(authenticated.User);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+}
---
+++ projects/RandoMainDLL/UDPSocketClient.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Threading;
+using Google.Protobuf;
+using Network;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace RandoMainDLL {
+  public static class UDPSocketClient {
+    public static BlockingCollection<Packet> SendQueue = new BlockingCollection<Packet>();
+
+    public static string Domain { get => AHK.IniString("Paths", "URL", "wotw.orirando.com"); }
+
+    private static UdpClient client;
+    private static byte[] udpId;
+    private static byte[] udpKey;
+    private static readonly int port = 31415;
+    private static Thread updateThread;
+    private static Mutex mutex;
+
+    public static bool IsStarted { get; private set; }
+
+    public static void Start(int id, byte[] key) {
+      mutex.WaitOne();
+      udpId = BitConverter.GetBytes(id);
+      udpKey = key;
+      mutex.ReleaseMutex();
+
+      if (IsStarted)
+        return;
+
+      client = new UdpClient(port);
+      try {
+        client.Connect(Domain, port);
+        IsStarted = true;
+        IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        setupUpdateThread();
+      }
+      catch (Exception e) {
+        Console.WriteLine(e.ToString());
+      }
+    }
+
+    public static void Stop() {
+      if (IsStarted)
+        client.Close();
+    }
+
+    private static TimeSpan time;
+    private static void setupUpdateThread() {
+      if (updateThread == null) {
+        time = DateTime.Now.TimeOfDay;
+        updateThread = new Thread(() => {
+          Task<UdpReceiveResult> task = client.ReceiveAsync();
+          while (true) {
+            if (IsStarted) {
+              try {
+                mutex.WaitOne();
+                var id = udpId;
+                var key = udpKey;
+                mutex.ReleaseMutex();
+
+                // Send
+                if ((DateTime.Now.TimeOfDay - time).TotalSeconds >= 20) {
+                  time = DateTime.Now.TimeOfDay;
+                  client.Send(new byte[0], 0);
+                }
+
+                Packet packet;
+                if (SendQueue.TryTake(out packet)) {
+                  var data = packet.ToByteArray();
+                  for (var i = 0; i < data.Length; ++i)
+                    data[i] = (byte)(data[i] ^ key[i % key.Length]);
+
+                  data = id.Concat(data).ToArray();
+                  client.Send(data, data.Length);
+                }
+
+                // Receive
+                if (task.IsCompleted || task.IsCanceled) {
+                  if (task.IsCompleted) {
+                    var data = task.Result.Buffer;
+                    for (var i = 0; i < data.Length; ++i)
+                      data[i] = (byte)(data[i] ^ key[i % key.Length]);
+
+                    HandleMessage(data);
+                  }
+
+                  task = client.ReceiveAsync();
+                  task.Start();
+                }
+                else if (task.IsFaulted) {
+                  throw task.Exception;
+                }
+              }
+              catch (Exception e) {
+                Randomizer.Warn("UDPSocket.UpdateThread", $"caught error {e}");
+              }
+            }
+          }
+        });
+        updateThread.Start();
+      }
+    }
+
+    public static void SendPlayerPosition(float x, float y) {
+      try {
+        var playerPosition = new PlayerPositionMessage();
+        playerPosition.X = x;
+        playerPosition.Y = y;
+        Packet packet = new Packet {
+          Id = 9,
+          Packet_ = playerPosition.ToByteString()
+        };
+        SendQueue.Add(packet);
+      }
+      catch (Exception e) { Randomizer.Error("SendPlayerPosition", e, false); }
+    }
+
+    private static void HandleMessage(byte[] data) {
+      try {
+        if (data == null) {
+          Randomizer.Log("received message with no data!");
+          return;
+        }
+        if (data.Length > 0) {
+          var packet = Packet.Parser.ParseFrom(data);
+          switch (packet.Id) {
+            case 11:
+              Multiplayer.Queue.Add(packet);
+              break;
+            default:
+              break;
+          }
+        }
+      }
+      catch (Exception t) {
+        Randomizer.Error("t", t);
+      }
+    }
+  }
+}
