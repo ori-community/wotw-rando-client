8a0cb3068bbd0a54a608c70a083bf50ada86f84c
MunWolf <badulf96@gmail.com>
2021-10-23 22:06:24 +0100

    Remove atrocities to humanity.

--- projects/RandoMainDLL/SeedController.cs
+++ projects/RandoMainDLL/SeedController.cs
@@ -175,16 +175,6 @@ namespace RandoMainDLL {
             var cond = new UberStateCondition(frags[0].ParseToInt(), frags[1]);
             var pickupType = (PickupType)frags[2].ParseToByte();
 
-            // legacy shop cost support
-            if (pickupType != PickupType.UberState && cond.Id.GroupID == (int)FakeUberGroups.OPHER_WEAPON && cond.Id.ID < 10000 && frags.Count() > 4 && float.TryParse(frags.Last(), NumberStyles.Number, CultureInfo.GetCultureInfo("en-US"), out float oMulti)) {
-              ((AbilityType)cond.Id.ID).Slot().CostMultiplier = oMulti + 1;
-              frags.RemoveAt(frags.Count() - 1);
-            }
-            if (pickupType != PickupType.UberState && cond.Id.GroupID == (int)FakeUberGroups.TWILLEN_SHARD && cond.Id.ID < 100 && frags.Count() > 4 && float.TryParse(frags.Last(), NumberStyles.Number, CultureInfo.GetCultureInfo("en-US"), out float tMulti)) {
-              ((ShardType)cond.Id.ID).Slot().CostMultiplier = tMulti + 1;
-              frags.RemoveAt(frags.Count() - 1);
-            }
-
             var extras = frags.Skip(4).ToList();
             bool needsMute = false;
             if (pickupType != PickupType.Message && extras.Contains("mute")) {
