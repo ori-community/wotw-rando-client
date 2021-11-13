5f73200fd51d0540afaa545486a9c404a29ca156
MunWolf <badulf96@gmail.com>
2021-11-13 20:21:29 +0000

    Give a buffer for left right if flag was set to false right before launching.

--- projects/InjectDLL/features/launch_no_deceleration.cpp
+++ projects/InjectDLL/features/launch_no_deceleration.cpp
@@ -13,14 +13,16 @@ using namespace modloader;
 namespace
 {
     constexpr float NO_AIR_DECELERATION_DURATION = 0.2f;
+    constexpr float NO_AIR_DECELERATION_RESET_DURATION = 0.1f;
     float aim_timer = 0.0f;
+    float reset_timer = 0.0f;
 
     STATIC_IL2CPP_BINDING(Game, UI, bool, get_MainMenuVisible, ());
     STATIC_IL2CPP_BINDING(Game, UI, bool, get_WorldMapVisible, ());
     STATIC_IL2CPP_BINDING(Game, UI, bool, get_ShardShopVisible, ());
     STATIC_IL2CPP_BINDING(Game, UI, bool, IsInventoryVisible, ());
     STATIC_IL2CPP_BINDING(, TimeUtility, float, get_deltaTime, ());
-    bool is_aiming_launch()
+    bool is_aiming_launch(app::CharacterAirNoDeceleration* this_ptr)
     {
         auto in_menu = il2cpp::get_class<app::UI__Class>("Game", "UI")->static_fields->m_sMenu->fields.m_equipmentWhellVisible;
         in_menu |= UI::get_MainMenuVisible();
@@ -28,12 +30,21 @@ namespace
         in_menu |= UI::get_ShardShopVisible();
         in_menu |= UI::IsInventoryVisible();
         if (!in_menu)
-            aim_timer -= TimeUtility::get_deltaTime();
+        {
+            if (aim_timer >= 0.0f)
+                aim_timer -= TimeUtility::get_deltaTime();
+            if (reset_timer >= 0.0f)
+                reset_timer -= TimeUtility::get_deltaTime();
+        }
 
         auto* sein = get_sein();
         auto* wrapper = sein->fields.Abilities->fields.ChargeJumpWrapper;
         if (wrapper->fields.HasState && wrapper->fields.State->fields.m_state == app::SeinChargeJump_State__Enum_Aiming)
+        {
             aim_timer = NO_AIR_DECELERATION_DURATION;
+            if (reset_timer > 0.0f)
+                this_ptr->fields.m_noDeceleration = true;
+        }
 
         return aim_timer > 0.0f;
     }
@@ -52,9 +63,14 @@ namespace
 
             auto* left_right_movement = platform_behaviour->fields.LeftRightMovement;
             if (!left_right_movement->fields.m_settings->fields.LockInput &&
-                !is_aiming_launch() &&
+                !is_aiming_launch(this_ptr) &&
                 left_right_movement->fields.m_horizontalInput != 0.0)
+            {
+                if (this_ptr->fields.m_noDeceleration)
+                    reset_timer = NO_AIR_DECELERATION_RESET_DURATION;
+
                 this_ptr->fields.m_noDeceleration = false;
+            }
         }
     }
 }
