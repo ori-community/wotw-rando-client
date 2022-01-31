d39b56e9439bd295b52b478ab7d27af96081aeb3
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2022-01-31 15:44:14 +0100

    this name was really confusing maybe this will make it easier for the next person

--- projects/InjectDLL/uber_states/uber_state_manager.cpp
+++ projects/InjectDLL/uber_states/uber_state_manager.cpp
@@ -328,7 +328,7 @@ namespace uber_states
                     add_state<app::SerializedBooleanUberState>("SerializedBooleanUberState", constants::RANDO_CONFIG_GROUP_NAME, constants::RANDO_CONFIG_GROUP_ID, "enable_vanilla_regen_tree", 4, false),
                     add_state<app::SerializedBooleanUberState>("SerializedBooleanUberState", constants::RANDO_CONFIG_GROUP_NAME, constants::RANDO_CONFIG_GROUP_ID, "disable_tree_check_for_rain", 5, false),
 
-                    add_state<app::SerializedBooleanUberState>("SerializedBooleanUberState", constants::MAP_FILTER_GROUP_NAME, constants::MAP_FILTER_GROUP_ID, "show_spoiler", 70, false),
+                    add_state<app::SerializedBooleanUberState>("SerializedBooleanUberState", constants::MAP_FILTER_GROUP_NAME, constants::MAP_FILTER_GROUP_ID, "spoiler_icon_tag", 70, false),
 
                     add_state<app::SerializedFloatUberState>("SerializedFloatUberState", constants::RANDO_STATS_GROUP_NAME, constants::RANDO_STATS_GROUP_ID, "Marsh Time", 0, 0),
                     add_state<app::SerializedFloatUberState>("SerializedFloatUberState", constants::RANDO_STATS_GROUP_NAME, constants::RANDO_STATS_GROUP_ID, "Hollow Time", 1, 0),
