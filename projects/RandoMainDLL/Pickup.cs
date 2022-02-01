c57b9d7ad57b11a5d01e3b3e946c0c65128dbd29
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2022-02-01 20:14:56 +0100

    yep

--- projects/RandoMainDLL/Pickup.cs
+++ projects/RandoMainDLL/Pickup.cs
@@ -146,6 +146,7 @@ namespace RandoMainDLL {
     MapRelicList = 1,
     PickupCount = 2,
     GoalProgress = 3,
+    ListHint = 4,
   }
 
   public abstract class Pickup {
@@ -1093,7 +1094,10 @@ namespace RandoMainDLL {
     public readonly SysMessageType messageType;
     public readonly string extraData;
 
-    public static Pickup Build(SysMessageType mt, string extra = "") => new SysMessage(mt, extra);
+    public static Pickup Build(SysMessageType mt, string extra = "") {
+      if (mt == SysMessageType.ListHint) return new ListHint(mt, extra);
+      return new SysMessage(mt, extra);
+    }
     public SysMessage(SysMessageType mt, string extra = "") {
       messageType = mt;
       extraData = extra;
