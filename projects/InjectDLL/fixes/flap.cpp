696cf106bb8a69f1e7f775da17c83e8b952aa052
MunWolf <badulf96@gmail.com>
2022-02-22 08:13:22 +0000

    fixes #235

--- projects/InjectDLL/fixes/flap.cpp
+++ projects/InjectDLL/fixes/flap.cpp
@@ -1,5 +1,6 @@
 #include <constants.h>
 #include <dll_main.h>
+#include <uber_states/uber_state_manager.h>
 
 #include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception.h>
@@ -9,6 +10,13 @@ bool temporary_glide_switch = false;
 
 namespace
 {
+    IL2CPP_INTERCEPT(, SeinGlide, bool, get_CanGlide, (app::SeinGlide* this_ptr)) {
+        if (temporary_glide_switch)
+            return false;
+
+        return SeinGlide::get_CanGlide(this_ptr);
+    }
+
     IL2CPP_INTERCEPT(, SeinFeatherFlap, void, EnterAttack, (app::SeinFeatherFlap* this_ptr)) {
         if (!has_ability(app::AbilityType__Enum_Glide))
         {
@@ -23,7 +31,10 @@ namespace
         SeinFeatherFlap::ExitAttack(this_ptr);
         if (temporary_glide_switch)
         {
-            set_ability(app::AbilityType__Enum_Glide, false);
+            // If we picked up glide in the meantime don't remove it.
+            if (uber_states::get_uber_state_value(6, 1014) < 0.5)
+                set_ability(app::AbilityType__Enum_Glide, false);
+
             temporary_glide_switch = false;
         }
     }
