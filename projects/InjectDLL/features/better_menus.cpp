ce8821a0c26b7992c37767cd6d9854d0cd705ee6
Timo Schwarzer <timo@schwarzer.dev>
2022-06-01 20:58:51 +0200

    Fix game getting broken when QTM
    Calling LoadMenus at specific points in time corrupts the game for some reason

--- projects/InjectDLL/features/better_menus.cpp
+++ projects/InjectDLL/features/better_menus.cpp
@@ -1,19 +1,22 @@
 #include <Common/ext.h>
 
 #include <Il2CppModLoader/common.h>
+#include <Il2CppModLoader/console.h>
 #include <Il2CppModLoader/interception_macros.h>
 
+#include <enums/game_event.h>
+#include <event_bus.h>
+#include <game/game.h>
+#include <uber_states/uber_state_helper.h>
+
 using namespace modloader;
+using modloader::console::console_send;
+using modloader::console::console_flush;
 
 namespace
 {
     // Skip fade to black when opening menus
     bool skip_fade_to_black = false;
-
-    IL2CPP_INTERCEPT(, MenuScreenManager, void, ShowMenuScreen,
-                       (app::MenuScreenManager * this_ptr, app::MenuScreenManager_Screens__Enum screen, bool immediate, bool playSound, bool pause)) {
-        MenuScreenManager::ShowMenuScreen(this_ptr, screen, immediate, playSound, pause);
-    }
 
     IL2CPP_INTERCEPT(, FaderB, void, Fade, (
             app::FaderB * this_ptr, float fadeInDuration, float fadeStayDuration, float fadeOutDuration, app::Action * fadeInComplete,
@@ -31,12 +34,5 @@ namespace
         ScopedSetter setter(skip_fade_to_black, true);
         MenuScreenManager_PostFadeMenuOpen_d_100_MoveNext(this_ptr);
         return MenuScreenManager_PostFadeMenuOpen_d_100_MoveNext(this_ptr);
-    }
-
-    IL2CPP_BINDING(, MenuScreenManager, void, LoadMenus, (app::MenuScreenManager* this_ptr, app::Action* onMenusLoaded))
-    IL2CPP_INTERCEPT(, MenuScreenManager, void, RemoveGameplayObjects, (app::MenuScreenManager* this_ptr)) {
-        // This unloads menus, so we just load them again immediately
-        MenuScreenManager::RemoveGameplayObjects(this_ptr);
-        MenuScreenManager::LoadMenus(this_ptr, nullptr);
     }
 }
