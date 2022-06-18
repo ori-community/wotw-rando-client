754323075f54527d2a81b575bb9eca5c87267256
Timo Schwarzer <timo@schwarzer.dev>
2022-06-18 02:32:33 +0200

    Implement hold to skip dialogue

---
+++ projects/InjectDLL/features/controls/hold_to_skip_dialog.cpp
@@ -0,0 +1,111 @@
+#include <Common/ext.h>
+#include <Il2CppModLoader/app/methods/InteractiveMessageBox.h>
+#include <Il2CppModLoader/app/methods/NPCMessageBox.h>
+#include <Il2CppModLoader/app/methods/MessageBox.h>
+#include <Il2CppModLoader/app/methods/TimeUtility.h>
+#include <Il2CppModLoader/common.h>
+#include <Il2CppModLoader/il2cpp_helpers.h>
+#include <Il2CppModLoader/interception_macros.h>
+#include <enums/game_event.h>
+#include <event_bus.h>
+#include <game/game.h>
+
+using namespace app::methods;
+
+namespace {
+    constexpr float DIALOGUE_SKIP_INITIAL_INTERVAL = 0.2f;
+    constexpr float DIALOGUE_SKIP_INTERACTION_INTERVAL = 0.4f;
+    constexpr float DIALOGUE_SKIP_INTERVAL = 0.02f;
+
+    float next_dialogue_skip_timeout = DIALOGUE_SKIP_INITIAL_INTERVAL;
+
+    bool can_do_dialogue_skip() {
+        return next_dialogue_skip_timeout < FLT_EPSILON;
+    }
+
+    bool should_do_dialogue_skip() {
+        if (next_dialogue_skip_timeout < FLT_EPSILON) {
+            next_dialogue_skip_timeout = DIALOGUE_SKIP_INTERVAL;
+            return true;
+        }
+
+        return false;
+    }
+
+    void on_fixed_update(GameEvent game_event, EventTiming timing) {
+        auto input_cmd = il2cpp::get_nested_class<app::Input_Cmd__Class>("Core", "Input", "Cmd");
+        auto skip_pressed = input_cmd->static_fields->DialogueAdvance->fields.IsPressed;
+
+        if (!skip_pressed) {
+            next_dialogue_skip_timeout = DIALOGUE_SKIP_INITIAL_INTERVAL;
+            return;
+        }
+
+        next_dialogue_skip_timeout = std::max(0.f, next_dialogue_skip_timeout - TimeUtility::get_fixedDeltaTime());
+    }
+
+    void initialize() {
+        game::event_bus().register_handler(GameEvent::FixedUpdate, EventTiming::Start, &on_fixed_update);
+    }
+
+    CALL_ON_INIT(initialize);
+
+    IL2CPP_INTERCEPT(NPCMessageBox, void, UpdateWriting, (app::NPCMessageBox* this_ptr)) {
+        auto previous_state = this_ptr->fields.m_state;
+
+        next::NPCMessageBox::UpdateWriting(this_ptr);
+
+        auto new_state = this_ptr->fields.m_state;
+
+        if (
+                previous_state != app::NPCMessageBox_State__Enum::Completed &&
+                new_state == app::NPCMessageBox_State__Enum::Completed &&
+                (
+                        this_ptr->fields.XInteractionBinding != nullptr ||
+                        this_ptr->fields.YInteractionBinding != nullptr ||
+                        this_ptr->fields.AInteractionBinding != nullptr ||
+                        this_ptr->fields.BInteractionBinding != nullptr
+                )
+        ) {
+            next_dialogue_skip_timeout = DIALOGUE_SKIP_INTERACTION_INTERVAL;
+        }
+    }
+
+    IL2CPP_INTERCEPT(NPCMessageBox, void, FixedUpdate, (app::NPCMessageBox* this_ptr)) {
+        if (this_ptr->fields.m_state == app::NPCMessageBox_State__Enum::Writing && should_do_dialogue_skip()) {
+            MessageBox::FinishWriting(this_ptr->fields.MessageBox);
+        } else if (this_ptr->fields.m_state == app::NPCMessageBox_State__Enum::Completed && should_do_dialogue_skip()) {
+            auto input_cmd = il2cpp::get_nested_class<app::Input_Cmd__Class>("Core", "Input", "Cmd");
+            if (input_cmd->static_fields->DialogueOption1->fields.IsPressed && this_ptr->fields.XInteractionBinding != nullptr) {
+                NPCMessageBox::EndMessageBox(this_ptr, this_ptr->fields.XInteractionBinding->fields.Action);
+                input_cmd->static_fields->DialogueOption1->fields.Used = true;
+
+            } else if (input_cmd->static_fields->DialogueOption2->fields.IsPressed && this_ptr->fields.YInteractionBinding != nullptr) {
+                NPCMessageBox::EndMessageBox(this_ptr, this_ptr->fields.YInteractionBinding->fields.Action);
+                input_cmd->static_fields->DialogueOption2->fields.Used = true;
+
+            } else if (input_cmd->static_fields->DialogueOption3->fields.IsPressed && this_ptr->fields.AInteractionBinding != nullptr) {
+                NPCMessageBox::EndMessageBox(this_ptr, this_ptr->fields.AInteractionBinding->fields.Action);
+                input_cmd->static_fields->DialogueOption3->fields.Used = true;
+
+            } else if (input_cmd->static_fields->DialogueExit->fields.IsPressed && this_ptr->fields.BInteractionBinding != nullptr) {
+                NPCMessageBox::EndMessageBox(this_ptr, this_ptr->fields.BInteractionBinding->fields.Action);
+                input_cmd->static_fields->DialogueExit->fields.Used = true;
+            }
+
+            NPCMessageBox::UpdateComplete(this_ptr);
+        }
+
+        next::NPCMessageBox::FixedUpdate(this_ptr);
+    }
+
+    IL2CPP_INTERCEPT(InteractiveMessageBox, void, FixedUpdate, (app::InteractiveMessageBox* this_ptr)) {
+        if (this_ptr->fields.m_state == app::InteractiveMessageBox_State__Enum::Writing && should_do_dialogue_skip()) {
+            MessageBox::FinishWriting(this_ptr->fields.MessageBox);
+        } else if (this_ptr->fields.m_state == app::InteractiveMessageBox_State__Enum::Completed && should_do_dialogue_skip()) {
+            InteractiveMessageBox::Complete(this_ptr);
+        }
+
+        next::InteractiveMessageBox::FixedUpdate(this_ptr);
+    }
+}
