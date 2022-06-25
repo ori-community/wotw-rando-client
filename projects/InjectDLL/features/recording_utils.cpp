bc124b9b373aecb39a5b64b19ad105c74c24cd9d
Timo Schwarzer <timo@schwarzer.dev>
2022-06-25 20:59:03 +0200

    Split up recording utils into multiple commands

--- projects/InjectDLL/features/recording_utils.cpp
+++ projects/InjectDLL/features/recording_utils.cpp
@@ -6,6 +6,7 @@
 #include <Il2CppModLoader/app/methods/Moon/Rendering/ShaderTime.h>
 #include <Il2CppModLoader/app/methods/UberPostProcess.h>
 #include <Il2CppModLoader/app/methods/UnityEngine/Camera.h>
+#include <Il2CppModLoader/app/methods/UnityEngine/Color.h>
 #include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception_macros.h>
 #include <Il2CppModLoader/windows_api/console.h>
@@ -15,13 +16,13 @@ using namespace modloader::win;
 
 namespace {
     app::Color default_background_color;
-    bool disable_vignette = false;
+    bool enable_vignette = true;
     bool camera_locked = false;
 
     IL2CPP_INTERCEPT(UberPostProcess, void, ApplySettings_2, (app::UberPostProcess * this_ptr, app::CameraSettings* settingsAsset)) {
         next::UberPostProcess::ApplySettings_2(this_ptr, settingsAsset);
 
-        if (disable_vignette) {
+        if (!enable_vignette) {
             this_ptr->fields.Vignetting->fields.Intensity = 0.0f;
             this_ptr->fields.Vignetting->fields.SecondVignetteAmount = 0.0f;
         }
@@ -39,61 +40,94 @@ namespace {
         }
     }
 
+    bool read_single_bool_from_command(std::vector<console::CommandParam> const& params, bool& boolean) {
+        if (params.size() != 1) {
+            console::console_send("Invalid number of arguments. Expected 1");
+            return false;
+        }
+
+        if (!console::try_get_bool(params[0], boolean)) {
+            console::console_send("Invalid argument. Expected boolean (on/off)");
+            return false;
+        }
+
+        return true;
+    }
+
     void set_background_color(app::Color color) {
         auto camera = UnityEngine::Camera::get_main();
         UnityEngine::Camera::set_backgroundColor(camera, color);
     }
 
-    void set_recording_mode(std::string const& command, std::vector<console::CommandParam> const& params) {
-        if (params.size() != 1) {
-            console::console_send("Invalid number of arguments. Expected 1");
+    void set_background_color_command(std::string const& command, std::vector<console::CommandParam> const& params) {
+        if (params.size() != 3) {
+            console::console_send("Invalid number of arguments. Expected 3");
             return;
         }
 
-        bool enable = false;
-        if (!console::try_get_bool(params[0], enable)) {
-            console::console_send("Invalid argument. Expected boolean (on/off)");
+        float r = 0.f;
+        float g = 0.f;
+        float b = 0.f;
+
+        if (!console::try_get_float(params[0], r)) {
+            console::console_send(format("Invalid argument. Expected float, got '%s'", params[0].value.data()));
             return;
         }
 
-        if (enable) {
-            disable_vignette = true;
-            set_background_color(app::Color{ 0.f, 0.f, 0.f });
-            Moon::Rendering::ShaderTime::set_freezeCameraSwaying(true);
-        } else {
-            disable_vignette = false;
-            set_background_color(default_background_color);
-            Moon::Rendering::ShaderTime::set_freezeCameraSwaying(false);
+        if (!console::try_get_float(params[1], r)) {
+            console::console_send(format("Invalid argument. Expected float, got '%s'", params[1].value.data()));
+            return;
         }
 
-        auto const gameplay_camera = il2cpp::get_nested_class<app::UI_Cameras__Class>("Game", "UI", "Cameras")->static_fields->Current;
-        auto const camera_post_processing = GameplayCamera::get_CameraPostProcessing(gameplay_camera);
-        auto uber_post_process = camera_post_processing->fields.UberPostProcess;
-        UberPostProcess::ApplySettings_2(uber_post_process, CameraPostProcessing::get_CameraSettingsToUse(camera_post_processing));
+        if (!console::try_get_float(params[2], r)) {
+            console::console_send(format("Invalid argument. Expected float, got '%s'", params[2].value.data()));
+            return;
+        }
 
-        console::console_send(format("Recording mode %s", enable ? "enabled" : "disabled"));
+        set_background_color(app::Color {r, g, b});
+
+        console::console_send("Background color set");
     }
 
-    void set_camera_locked(std::string const& command, std::vector<console::CommandParam> const& params) {
-        if (params.size() != 1) {
-            console::console_send("Invalid number of arguments. Expected 1");
-            return;
-        }
+    void reset_background_color_command(std::string const& command, std::vector<console::CommandParam> const& params) {
+        set_background_color(default_background_color);
+        console::console_send("Background color reset");
+    }
 
-        if (!console::try_get_bool(params[0], camera_locked)) {
-            console::console_send("Invalid argument. Expected boolean (on/off)");
-            return;
-        }
+    void set_vignette_command(std::string const& command, std::vector<console::CommandParam> const& params) {
+        if (read_single_bool_from_command(params, enable_vignette)) {
+            auto const gameplay_camera = il2cpp::get_nested_class<app::UI_Cameras__Class>("Game", "UI", "Cameras")->static_fields->Current;
+            auto const camera_post_processing = GameplayCamera::get_CameraPostProcessing(gameplay_camera);
+            auto uber_post_process = camera_post_processing->fields.UberPostProcess;
+            UberPostProcess::ApplySettings_2(uber_post_process, CameraPostProcessing::get_CameraSettingsToUse(camera_post_processing));
 
-        console::console_send(format("Camera %s", camera_locked ? "locked" : "unlocked"));
+            console::console_send(format("Vignette %s", enable_vignette ? "enabled" : "disabled"));
+        }
+    }
+
+    void set_camera_swaying_command(std::string const& command, std::vector<console::CommandParam> const& params) {
+        bool enable = false;
+        if (read_single_bool_from_command(params, enable)) {
+            Moon::Rendering::ShaderTime::set_freezeCameraSwaying(enable);
+            console::console_send(format("Recording mode %s", enable ? "enabled" : "disabled"));
+        }
+    }
+
+    void set_camera_locked_command(std::string const& command, std::vector<console::CommandParam> const& params) {
+        if (read_single_bool_from_command(params, camera_locked)) {
+            console::console_send(format("Camera %s", camera_locked ? "locked" : "unlocked"));
+        }
     }
 
     void initialize() {
         auto camera = UnityEngine::Camera::get_main();
         default_background_color = UnityEngine::Camera::get_backgroundColor(camera);
 
-        console::register_command({ "recording_utils", "set_recording_mode_enabled" }, set_recording_mode);
-        console::register_command({ "recording_utils", "set_camera_locked" }, set_camera_locked);
+        console::register_command({ "recording_utils", "set_background_color" }, set_background_color_command);
+        console::register_command({ "recording_utils", "reset_background_color" }, reset_background_color_command);
+        console::register_command({ "recording_utils", "set_vignette" }, set_vignette_command);
+        console::register_command({ "recording_utils", "set_camera_swaying" }, set_camera_swaying_command);
+        console::register_command({ "recording_utils", "set_camera_locked" }, set_camera_locked_command);
     }
 
     CALL_ON_INIT(initialize);
