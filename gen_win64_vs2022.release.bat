cb91d5b755bbdb41442efc671c7e66632739a297
Timo Schwarzer <timo@schwarzer.dev>
2022-06-27 22:39:19 +0200

    Update cmake gen scripts

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,13 +1,12 @@
 @echo off
 title gen-win64
 echo Generating visual studio solution.
-rd /s /q "./build"
 mkdir build
 cd build
 mkdir win64
 cd win64
 
-cmake -DARCH=x64 -DDEFINE_INTERNALS=true -G "Visual Studio 16 2019" ..\..
+cmake -DARCH=x64 -G "Visual Studio 17 2022" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="/MP"  ..\..
 if %errorlevel% equ 0 (
 	dotnet restore ./WotWRandomizer.sln -r win-x64
 	dotnet sln ./projects/Injector/Injector.sln remove ./projects/Injector/ALL_BUILD.vcxproj
