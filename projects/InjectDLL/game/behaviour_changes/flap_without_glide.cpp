56cdbd8300383c3f776734047d05211632e854b0
Timo Schwarzer <timo@schwarzer.dev>
2022-08-04 12:20:18 +0200

    Only grant Glide for the smallest amount of the subframe when using Flap
    This fixes triggering Glide cutscenes by using Flap

--- projects/InjectDLL/game/behaviour_changes/flap_without_glide.cpp
+++ projects/InjectDLL/game/behaviour_changes/flap_without_glide.cpp
@@ -1,58 +1,18 @@
 #include <constants.h>
-#include <game/game.h>
 #include <game/player.h>
-#include <uber_states/uber_state_interface.h>
 
-#include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception.h>
 #include <Il2CppModLoader/interception_macros.h>
-#include <Il2CppModLoader/app/methods/SeinGlide.h>
-#include <Il2CppModLoader/app/methods/SeinFeatherFlap.h>
+#include <Il2CppModLoader/app/methods/WindCollision.h>
+
+using namespace modloader;
+using namespace app::methods;
 
 namespace {
-    bool temporary_glide_switch = false;
-
-    IL2CPP_INTERCEPT(SeinGlide, bool, get_CanGlide, (app::SeinGlide * this_ptr)) {
-        if (temporary_glide_switch)
-            return false;
-
-        return next::SeinGlide::get_CanGlide(this_ptr);
+    IL2CPP_INTERCEPT(WindCollision, void, OnTriggerEnter, (app::WindCollision* this_ptr, app::Collider* collider)) {
+        auto previous_glide_state = game::player::has_ability(app::AbilityType__Enum::Glide);
+        game::player::set_ability(app::AbilityType__Enum::Glide, true);
+        next::WindCollision::OnTriggerEnter(this_ptr, collider);
+        game::player::set_ability(app::AbilityType__Enum::Glide, previous_glide_state);
     }
-
-    IL2CPP_INTERCEPT(SeinFeatherFlap, void, EnterAttack, (app::SeinFeatherFlap * this_ptr)) {
-        if (!game::player::has_ability(app::AbilityType__Enum::Glide)) {
-            temporary_glide_switch = true;
-            game::player::set_ability(app::AbilityType__Enum::Glide, true);
-        }
-
-        next::SeinFeatherFlap::EnterAttack(this_ptr);
-    }
-
-    uber_states::UberState glide(UberStateGroup::RandoState, 1014);
-    IL2CPP_INTERCEPT(SeinFeatherFlap, void, ExitAttack, (app::SeinFeatherFlap * this_ptr)) {
-        next::SeinFeatherFlap::ExitAttack(this_ptr);
-        if (temporary_glide_switch) {
-            // If we picked up glide in the meantime don't remove it.
-            if (!glide.get<bool>())
-                game::player::set_ability(app::AbilityType__Enum::Glide, false);
-
-            temporary_glide_switch = false;
-        }
-    }
-
-    void handle_enter_switches() {
-        if (temporary_glide_switch)
-            game::player::set_ability(app::AbilityType__Enum::Glide, false);
-    }
-
-    void handle_exit_switches() {
-        if (temporary_glide_switch)
-            game::player::set_ability(app::AbilityType__Enum::Glide, true);
-    }
-
-    void initialize() {
-        game::event_bus();
-    }
-
-    CALL_ON_INIT(initialize);
 } // namespace
