f31782e8238c14dbfa8d506c02f548d6146589e6
Timo Schwarzer <timo@schwarzer.dev>
2022-08-31 15:45:12 +0200

    Fix laser fix causing fps drops when many lasers are loaded

--- projects/InjectDLL/game/behaviour_changes/better_lasers.cpp
+++ projects/InjectDLL/game/behaviour_changes/better_lasers.cpp
@@ -2,6 +2,7 @@
 
 #include <Il2CppModLoader/app/methods/BlockableLaser.h>
 #include <Il2CppModLoader/app/methods/UnityEngine/GameObject.h>
+#include <Il2CppModLoader/app/methods/Damage.h>
 #include <Il2CppModLoader/common.h>
 #include <Il2CppModLoader/interception_macros.h>
 
@@ -15,19 +16,20 @@ namespace {
      * when they are offscreen.
      */
 
-    bool override_is_laser_on_screen = true;
-
     IL2CPP_INTERCEPT(BlockableLaser, bool, IsLaserOnScreen, (app::BlockableLaser * this_ptr, app::Vector3 end_point)) {
-        return override_is_laser_on_screen || next::BlockableLaser::IsLaserOnScreen(this_ptr, end_point);
+        return true;
     }
 
     IL2CPP_INTERCEPT(BlockableLaser, app::Damage*, DealLaserDamage, (app::BlockableLaser * this_ptr, app::GameObject* target)) {
-        ScopedSetter setter(override_is_laser_on_screen, false);
+        auto is_on_screen = next::BlockableLaser::IsLaserOnScreen(this_ptr, this_ptr->fields.m_cachedEnvHit.m_Point);
 
-        auto is_on_screen = BlockableLaser::IsLaserOnScreen(this_ptr, this_ptr->fields.m_cachedEnvHit.m_Point);
+        if (is_on_screen) {
+            return next::BlockableLaser::DealLaserDamage(this_ptr, target);
+        }
 
-        return is_on_screen
-                ? next::BlockableLaser::DealLaserDamage(this_ptr, target)
-                : nullptr;
+        auto damage = il2cpp::create_object<app::Damage>("", "Damage");
+        Damage::ctor(damage, 0.f, app::Vector2{ 0.f, 0.f }, app::Vector3{ 0.f, 0.f, 0.f }, app::DamageType__Enum::Laser, app::AbilityType__Enum::None, nullptr, 0, nullptr, app::SpiritShardType__Enum::None, true, app::DamageWeight__Enum::Default, 0.f, false);
+
+        return damage;
     }
 } // namespace
