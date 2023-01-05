b6b73396558ecfe299dc79d6286795a820f7968e
Timo Schwarzer <timo@schwarzer.dev>
2023-01-05 01:23:50 +0100

    Disable ability tracking when granting bash for deflector

--- projects/Randomizer/game/behaviour_changes/deflector_without_bash.cpp
+++ projects/Randomizer/game/behaviour_changes/deflector_without_bash.cpp
@@ -1,6 +1,8 @@
 #include <constants.h>
 #include <Core/api/game/player.h>
+#include <Randomizer/timing/game_timer.h>
 
+#include <Modloader/common.h>
 #include <Modloader/interception.h>
 #include <Modloader/interception_macros.h>
 #include <Modloader/app/methods/Projectile.h>
@@ -11,6 +13,7 @@ namespace {
         if (game::player::has_ability(app::AbilityType__Enum::Bash))
             ret = next::Projectile::HandleMirrorStrike(this_ptr, damage);
         else {
+            modloader::ScopedSetter setter(randomizer::timing::disable_ability_tracking, true);
             game::player::set_ability(app::AbilityType__Enum::Bash, true);
             ret = next::Projectile::HandleMirrorStrike(this_ptr, damage);
             game::player::set_ability(app::AbilityType__Enum::Bash, false);
