3c6744895cb1084803858c14612c32efc853c6a2
Timo Schwarzer <timo@schwarzer.dev>
2023-01-06 20:30:59 +0100

    Fix GameEvent::Teleport not triggering

--- projects/Randomizer/game/event_handlers/on_teleport.cpp
+++ projects/Randomizer/game/event_handlers/on_teleport.cpp
@@ -1,12 +1,14 @@
 #include <interop/csharp_bridge.h>
-#include <Randomizer/macros.h>
 
 #include <Modloader/interception_macros.h>
 #include <Modloader/app/methods/SavePedestalController.h>
 
+#include <Core/api/game/game.h>
+
 namespace {
     IL2CPP_INTERCEPT(SavePedestalController, void, OnFinishedTeleportingStartAnimation, ()) {
         csharp_bridge::on_teleporting();
+        game::event_bus().trigger_event(GameEvent::Teleport);
         next::SavePedestalController::OnFinishedTeleportingStartAnimation();
     }
 } // namespace
