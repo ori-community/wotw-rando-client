878a0794c1b34f31335dfb0f06095496b3527e61
Timo Schwarzer <timo@schwarzer.dev>
2023-12-03 20:06:03 +0100

    Use sccache as compilation cache

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake.exe -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 17 2022" -T ClangCL -B cmake-build-relwithdebinfo
+cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 17 2022" -T ClangCL -B cmake-build-relwithdebinfo
 pause
