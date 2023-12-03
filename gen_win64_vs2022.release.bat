64e0ff4dab79801e9155887d4661056549a28318
Timo Schwarzer <timo@schwarzer.dev>
2023-12-03 22:50:06 +0100

    Use some random action that sets some random environment variables

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,4 +1,3 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER="clang-cl.exe" -DCMAKE_CXX_COMPILER="clang-cl.exe" -G Ninja -B cmake-build-relwithdebinfo
 pause
