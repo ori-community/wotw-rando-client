8fe04ed4e6389ba763f1566ded62f452e98cae2c
Timo Schwarzer <timo@schwarzer.dev>
2023-12-03 22:27:27 +0100

    Use clang-cl from PATH

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -B cmake-build-relwithdebinfo
+cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER="clang-cl.exe" -DCMAKE_CXX_COMPILER="clang-cl.exe" -G Ninja -B cmake-build-relwithdebinfo
 pause
