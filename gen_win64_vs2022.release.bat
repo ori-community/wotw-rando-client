9a30e1a25433046a391e1d4a654875b02f0828d9
Timo Schwarzer <timo@schwarzer.dev>
2023-12-03 22:33:43 +0100

    Microsoft dark magic

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,3 +1,4 @@
 @echo off
-cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER="clang-cl.exe" -DCMAKE_CXX_COMPILER="clang-cl.exe" -G Ninja -B cmake-build-relwithdebinfo
+call "C:/Program Files (x86)/VC/vcvarsall.bat" amd64
+cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER:PATH="%ProgramFiles(x86)%\LLVM\bin\clang.exe" -DCMAKE_CXX_COMPILER:PATH="%ProgramFiles(x86)%\LLVM\bin\clang.exe" -G Ninja -B cmake-build-relwithdebinfo
 pause
