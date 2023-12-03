427ea6c648c60bba21f2e1db8ec509bcc5b2c804
Timo Schwarzer <timo@schwarzer.dev>
2023-12-03 22:12:39 +0100

    Remove static compiler paths

--- gen_win64_vs2022.release.bat
+++ gen_win64_vs2022.release.bat
@@ -1,3 +1,3 @@
 @echo off
-cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-cl.exe" -DCMAKE_CXX_COMPILER="C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin/clang-cl.exe" -G Ninja -B cmake-build-relwithdebinfo
+cmake.exe -DCMAKE_C_COMPILER_LAUNCHER=sccache -DCMAKE_CXX_COMPILER_LAUNCHER=sccache -DCMAKE_BUILD_TYPE=Debug -DCMAKE_BUILD_TYPE=RelWithDebInfo -G Ninja -B cmake-build-relwithdebinfo
 pause
