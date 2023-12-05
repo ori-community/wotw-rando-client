53ce361a22e32fb86a0e8379f6186197316a1d08
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2023-12-05 00:38:07 +0000

    Fix being unable to enter doors and talk to Father Moki.

--- projects/Randomizer/game/condition_intercepts/grom_projects.cpp
+++ projects/Randomizer/game/condition_intercepts/grom_projects.cpp
@@ -1,9 +1,10 @@
-#include <conditions/new_setup_state_override.h>
-#include <constants.h>
 
 #include <Core/api/uber_states/uber_state_handlers.h>
 
 #include <Modloader/modloader.h>
+
+#include <Randomizer/conditions/new_setup_state_override.h>
+#include <Randomizer/conditions/condition_uber_state.h>
 
 namespace {
     core::api::uber_states::UberState grom_spirit_well_built(UberStateGroup::GladesProjects, 16825);
@@ -30,17 +31,25 @@ namespace {
     );
 
     void create_build_intercept(core::api::uber_states::UberState state, const std::string& path) {
-        std::string actual_path = path;
         randomizer::conditions::register_new_setup_intercept(
             { path },
             { -151413539, 1018051603, -236466678 },
-            [state, actual_path](auto, auto, auto original_state, auto) -> int32_t {
-                modloader::info("test", actual_path);
+            [state](auto, auto, auto original_state, auto) -> int32_t {
                 if (state.get<bool>()) {
                     return -236466678;
                 }
 
                 return original_state == -236466678 ? 1018051603 : original_state;
+            }
+        );
+    }
+
+    void create_door_intercept(core::api::uber_states::UberState state, const int enabled, const int disabled, const std::string& path) {
+        randomizer::conditions::register_new_setup_intercept(
+            { path },
+            { enabled, disabled },
+            [state, enabled, disabled](auto, auto, auto, auto) -> int32_t {
+                return state.get<bool>() ? enabled : disabled;
             }
         );
     }
@@ -54,10 +63,15 @@ namespace {
         create_build_intercept(grom_open_cave_built, "wellspringGladesHubSetups/interactives/builderProjects/caveEntranceSetup");
         create_build_intercept(grom_beautify_built, "wellspringGladesHubSetups/interactives/builderProjects/beautifySetup");
 
+        create_door_intercept(grom_houses_a_built, 1123222511, -2139705815, "wellspringGladesHub/interactives/doorSetups/hutDoorsSetup");
+        create_door_intercept(grom_houses_b_built, -411183768, -1559429889, "wellspringGladesHubB/interactives/hutDoorSetup");
+        create_door_intercept(grom_houses_c_built, 1123222511, -2139705815, "wellspringGladesHub/interactives/doorSetups/hutDoorsSetupC");
+        create_door_intercept(grom_open_cave_built, -1148551599, 2133784501, "wellspringGladesHub/interactives/doorSetups/caveDoorSetup");
+
         randomizer::conditions::register_new_setup_intercept(
             { "wellspringGladesHubSetups/interactives/builderProjects/spiritWell" },
             { -1683158848, 1457677579 },
-            [](auto, auto, auto original_state, auto) -> int32_t {
+            [](auto, auto, auto, auto) -> int32_t {
                 return grom_spirit_well_built.get<bool>() ? 1457677579 : -1683158848;
             }
         );
@@ -65,8 +79,15 @@ namespace {
         randomizer::conditions::register_new_setup_intercept(
             { "swampIntroTop/artSetups/interactives/savePedestalSetup" },
             { 763396887, 800721598, -1090989360 },
-            [](auto, auto, auto original_state, auto) -> int32_t {
+            [](auto, auto, auto, auto) -> int32_t {
                 return grom_spirit_well_built.get<bool>() ? -1090989360 : 800721598;
+            }
+        );
+
+        randomizer::conditions::register_condition_uber_state_intercept(
+            core::api::uber_states::UberState(14019, 11894),
+            [](auto) {
+                return std::make_optional(grom_houses_a_built.get<bool>());
             }
         );
     });
