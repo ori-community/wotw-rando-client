f56698049135efac565abd7ac74111170bdfd3c1
Timo Schwarzer <timo@schwarzer.dev>
2023-12-14 00:06:30 +0100

    Fix flap being too strong and opening depths accidentally

--- projects/Randomizer/game/behaviour_changes/flap_without_glide.cpp
+++ projects/Randomizer/game/behaviour_changes/flap_without_glide.cpp
@@ -14,6 +14,8 @@ namespace {
     IL2CPP_INTERCEPT(WindCollision, void, OnTriggerEnter, (app::WindCollision * this_ptr, app::Collider* collider)) {
         ScopedSetter setter(randomizer::timing::disable_ability_tracking, true);
         const auto previous_glide_state = core::api::game::player::ability(app::AbilityType__Enum::Glide).get();
+
+        core::reactivity::ScopedTrackingBlocker _;
         core::api::game::player::ability(app::AbilityType__Enum::Glide).set(true);
         next::WindCollision::OnTriggerEnter(this_ptr, collider);
         core::api::game::player::ability(app::AbilityType__Enum::Glide).set(previous_glide_state);
