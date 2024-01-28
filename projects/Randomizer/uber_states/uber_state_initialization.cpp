7741fea84c7a43662a0c1d0622e5e09bd42d620f
LoadCake <stelod00@gmail.com>
2024-01-28 18:53:41 +0100

    I fix my horrendous issue I made yesterday

--- projects/Randomizer/uber_states/uber_state_initialization.cpp
+++ projects/Randomizer/uber_states/uber_state_initialization.cpp
@@ -295,6 +295,7 @@ namespace randomizer {
                 add_state<app::SerializedFloatUberState>(UberStateGroup::RandoUpgrade, "flashDamageMultiplier", 93, 1.0),
 
                 add_state<app::SerializedFloatUberState>(UberStateGroup::RandoUpgrade, "flashTickInterval", 94, 1.0),
+                add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoUpgrade, "shurikenBashable", 95, false),
 
                 add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoUpgrade, "overflowPickupUpgrade", 150, false),
 
