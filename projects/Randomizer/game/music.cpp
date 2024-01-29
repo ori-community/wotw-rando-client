5cc5116435531177175dda24aeaa8f48bf80ead8
Timo Schwarzer <me@timoschwarzer.com>
2024-01-29 19:13:33 +0100

    Groundwork for music modding framework

---
+++ projects/Randomizer/game/music.cpp
@@ -0,0 +1,16 @@
+#include <Core/dev/object_visualizer.h>
+#include <Modloader/app/methods/WotwUberStateWwiseStateManager.h>
+#include <Modloader/interception_macros.h>
+#include <Modloader/windows_api/console.h>
+#include <format>
+#include <fstream>
+
+using namespace app::classes;
+
+namespace {
+    IL2CPP_INTERCEPT(WotwUberStateWwiseStateManager, void, PopulateAffectedStates, (app::WotwUberStateWwiseStateManager * this_ptr )) {
+        // Modify this_ptr->fields.WotwUberStateToWwiseData->fields.InheritedMap here
+
+        next::WotwUberStateWwiseStateManager::PopulateAffectedStates(this_ptr);
+    }
+}
