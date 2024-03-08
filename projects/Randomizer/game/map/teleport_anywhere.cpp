c6437ab67a8a29098b81a7f6538f3a177166f364
Timo Schwarzer <me@timoschwarzer.com>
2024-03-08 16:35:12 +0100

    Don't allow teleport_anywhere if preventing debug

--- projects/Randomizer/game/map/teleport_anywhere.cpp
+++ projects/Randomizer/game/map/teleport_anywhere.cpp
@@ -1,3 +1,4 @@
+#include <Core/api/game/debug_menu.h>
 #include <Randomizer/game/map/teleport_anywhere.h>
 
 #include <Modloader/app/methods/RuntimeWorldMapIcon.h>
@@ -8,7 +9,7 @@ namespace randomizer::game::map {
 
     namespace {
         IL2CPP_INTERCEPT(RuntimeWorldMapIcon, bool, CanBeTeleportedTo, (app::RuntimeWorldMapIcon * this_ptr)) {
-            if (teleport_anywhere)
+            if (teleport_anywhere && core::api::game::debug_menu::is_preventing_enabling_debug())
                 return true;
 
             return next::RuntimeWorldMapIcon::CanBeTeleportedTo(this_ptr);
