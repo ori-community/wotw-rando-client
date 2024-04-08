22fe0bf06293cf8935a8b973f188266c5743a1b3
Timo Schwarzer <timo@schwarzer.dev>
2024-04-08 20:18:40 +0200

    Add uber state to remove sand from feeding grounds to elevator

---
+++ projects/Randomizer/features/scenes/modifications/sandless_feeding_grounds_to_elevator.cpp
@@ -0,0 +1,54 @@
+#include <Modloader/il2cpp_helpers.h>
+
+#include <Core/api/scenes/scene_load.h>
+#include <Core/api/uber_states/uber_state.h>
+#include <Core/enums/uber_state.h>
+#include <Core/property/reactivity.h>
+#include <Modloader/app/methods/ObjectInsideZoneChecker.h>
+#include <Modloader/windows_api/console.h>
+
+namespace {
+    using namespace app::classes;
+
+    std::vector<il2cpp::WeakGCRef<app::GameObject>> objects_to_disable;
+    std::shared_ptr<core::reactivity::ReactiveEffect> effect;
+
+    core::api::uber_states::UberState modification_enabled_state(UberStateGroup::RandoConfig, 101);
+
+    auto on_scene_loaded_handler = core::api::scenes::event_bus().register_handler(
+        [](auto metadata) {
+            if (metadata->state != app::SceneState__Enum::Enabled) {
+                return;
+            }
+
+            if (metadata->scene_name == "petrifiedOwlFeedingGroundsRevised") {
+                const auto scene_root_go = il2cpp::unity::get_game_object(metadata->scene->fields.SceneRoot);
+
+                objects_to_disable.clear();
+
+                objects_to_disable.emplace_back(il2cpp::unity::find_child(scene_root_go, std::vector<std::string> {"artSetups", "desertSandZoneTileA"}));
+                objects_to_disable.emplace_back(il2cpp::unity::find_child(scene_root_go, std::vector<std::string>{"artSetups", "sandZones", "sandZoneBaseB"}));
+                objects_to_disable.emplace_back(il2cpp::unity::find_child(scene_root_go, std::vector<std::string>{"earlyZParent", "earlyZMesh-regular-cell[68,26,0]-0"}));
+
+                effect = core::reactivity::watch_effect([] {
+                    const auto modification_enabled = modification_enabled_state.get<bool>();
+
+                    auto it = objects_to_disable.begin();
+                    while (it != objects_to_disable.end()) {
+                        if (it->is_valid()) {
+                            il2cpp::unity::set_active(**it, !modification_enabled);
+
+                            ++it;
+                        } else {
+                            it = objects_to_disable.erase(it); // Delete invalid references
+                        }
+                    }
+
+                    if (objects_to_disable.empty()) {
+                        effect = nullptr;
+                    }
+                });
+            }
+        }
+    );
+} // namespace
--- projects/Randomizer/uber_states/uber_state_initialization.cpp
+++ projects/Randomizer/uber_states/uber_state_initialization.cpp
@@ -379,6 +379,7 @@ namespace randomizer {
                 add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoConfig, "fixKwolokBossEscapeUnderwaterSpiritLightSoftlock", 15, false),
                 add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoConfig, "fixShopAnywhere", 16, false),
                 add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoConfig, "removeShriekEscapeSand", 100, false),
+                add_state<app::SerializedBooleanUberState>(UberStateGroup::RandoConfig, "removeFeedingGroundsToElevatorSand", 101, false),
 
                 add_state<app::SerializedIntUberState>(UberStateGroup::RandoConfig, "forceNoAirDecelerationFlag", FORCE_AIR_NO_DECELERATION_ID, 0),
 
