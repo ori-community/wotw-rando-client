66bc5e3bb59dfcabe4306b0949b5031d35572df3
Timo Schwarzer <timo@schwarzer.dev>
2024-04-09 22:54:28 +0200

    Fix meeting kwolok underwater cutscene trigger fix not being built

--- projects/Randomizer/features/scenes/modifications/kwolok_escape_sl_softlock_fix.cpp
+++ projects/Randomizer/features/scenes/modifications/kwolok_escape_sl_softlock_fix.cpp
@@ -14,7 +14,7 @@ namespace {
     std::optional<il2cpp::WeakGCRef<app::PlayerInsideZoneChecker>> player_inside_zone_checker;
     std::shared_ptr<core::reactivity::ReactiveEffect> effect;
 
-    core::api::uber_states::UberState fix_enabled_state(UberStateGroup::RandoConfig, 13);
+    core::api::uber_states::UberState fix_enabled_state(UberStateGroup::RandoConfig, 15);
 
     auto on_scene_loaded_handler = core::api::scenes::event_bus().register_handler(
         [](auto metadata) {
@@ -22,27 +22,30 @@ namespace {
                 return;
             }
 
-            if (metadata->scene_name == "kwoloksCavernThroneRoom") {
+            if (metadata->scene_name == "kwolokBossWispIntro") {
                 const auto scene_root_go = il2cpp::unity::get_game_object(metadata->scene->fields.SceneRoot);
 
-                const auto emerge_trigger_go = il2cpp::unity::find_child(
+                const auto zone_checker_go = il2cpp::unity::find_child(
                     scene_root_go,
                     std::vector<std::string>{
-                        "kwolokSetup",
-                        "emergeTrigger",
+                        "kwolokBoss",
+                        "prefightCheckers",
+                        "playerInsiderIntroZoneChecker",
                     }
                 );
 
                 player_inside_zone_checker = il2cpp::WeakGCRef(
-                    il2cpp::unity::get_component<app::PlayerInsideZoneChecker>(emerge_trigger_go, types::PlayerInsideZoneChecker::get_class())
+                    il2cpp::unity::get_component<app::PlayerInsideZoneChecker>(zone_checker_go, types::PlayerInsideZoneChecker::get_class())
                 );
 
                 effect = core::reactivity::watch_effect([] {
                     if (player_inside_zone_checker.has_value() && player_inside_zone_checker->is_valid()) {
                         if (fix_enabled_state.get<bool>()) {
-                            (**player_inside_zone_checker)->fields._.Anchor.y = 31.f;  // Fixed
+                            (**player_inside_zone_checker)->fields._.Anchor.y = 0.208f;  // Fixed
+                            (**player_inside_zone_checker)->fields._.Size.y = 0.5625f;  // Fixed
                         } else {
-                            (**player_inside_zone_checker)->fields._.Anchor.y = 14.6099997;  // Vanilla
+                            (**player_inside_zone_checker)->fields._.Anchor.y = 0.f;  // Vanilla
+                            (**player_inside_zone_checker)->fields._.Size.y = 1.f;  // Vanilla
                         }
 
                         ObjectInsideZoneChecker::UpdateBounds(reinterpret_cast<app::ObjectInsideZoneChecker*>(**player_inside_zone_checker));
