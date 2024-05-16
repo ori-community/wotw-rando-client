cb1d3673f8572e6da39f8f111267d38fd92f34b2
Timo Schwarzer <timo@schwarzer.dev>
2024-05-16 17:04:36 +0200

    Allow binding the same key to multiple actions in the vanilla controls menu

---
+++ projects/Randomizer/features/keyboard_multibind.cpp
@@ -0,0 +1,23 @@
+#include <Modloader/app/methods/ControlList.h>
+#include <Modloader/interception_macros.h>
+#include <Modloader/modloader.h>
+
+namespace {
+    // This patch allows binding the same key to
+    // multiple actions in the vanilla controls menu
+
+    auto is_assigning_key = false;
+
+    IL2CPP_INTERCEPT(ControlList, void, RemoveBinding, (app::ControlList * this_ptr, app::ControlItem* control, app::KeyCode__Enum code)) {
+        if (is_assigning_key) {
+            return;
+        }
+
+        next::ControlList::RemoveBinding(this_ptr, control, code);
+    }
+
+    IL2CPP_INTERCEPT(ControlList, void, AssignKey, (app::ControlList * this_ptr, app::ControlItem* control, int32_t index, app::KeyCode__Enum pressed_key)) {
+        modloader::ScopedSetter _(is_assigning_key, true);
+        next::ControlList::AssignKey(this_ptr, control, index, pressed_key);
+    }
+}
