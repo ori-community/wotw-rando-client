de21eeb2e6f7976ce2247be3ddb0f91c7fdec93d
Timo Schwarzer <timo@schwarzer.dev>
2024-05-20 13:22:18 +0200

    Fix the teleporter timer fix that was never a fix

--- projects/Core/api/game/in_game_timer.h
+++ projects/Core/api/game/in_game_timer.h
@@ -17,5 +17,4 @@ namespace core::api::game::in_game_timer {
 
     CORE_DLLEXPORT common::EventBus<TimeStep>& time_step_event_bus();
     CORE_DLLEXPORT AsyncLoadingState get_last_async_loading_state();
-    CORE_DLLEXPORT void set_minimum_teleportation_time(float time);
 }
