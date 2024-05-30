38bab44fdd49f1b07f16a937d765000688522304
Timo Schwarzer <timo@schwarzer.dev>
2024-05-30 09:16:45 +0200

    Add a custom animation/timeline for the bottom left heart boulder when having heart cutscenes disabled

--- projects/Randomizer/features/cutscene_skips/willow_heart_cutscenes.cpp
+++ projects/Randomizer/features/cutscene_skips/willow_heart_cutscenes.cpp
@@ -1,5 +1,7 @@
 #include <Modloader/app/methods/Moon/Timeline/TimelineEntity.h>
+#include <Modloader/app/methods/UnityEngine/GameObject.h>
 #include <Modloader/app/types/MoonTimeline.h>
+#include <Modloader/app/types/ChangeStateOnCondition.h>
 #include <Modloader/il2cpp_helpers.h>
 
 #include <Common/event_bus.h>
@@ -13,6 +15,9 @@ using namespace app::classes;
 
 namespace {
     std::vector<il2cpp::WeakGCRef<app::MoonTimeline>> vine_timelines;
+    std::optional<il2cpp::WeakGCRef<app::MoonTimeline>> vine_h_original_timeline_ref;
+    std::optional<il2cpp::WeakGCRef<app::MoonTimeline>> vine_h_rando_timeline_ref;
+    std::optional<il2cpp::WeakGCRef<app::ChangeStateOnCondition>> vine_h_change_state_on_condition_ref;
     std::shared_ptr<core::reactivity::ReactiveEffect> effect;
 
     core::api::uber_states::UberState disable_cutscenes_state(UberStateGroup::RandoConfig, 20);
@@ -35,7 +40,7 @@ namespace {
                 {"vineSetups", "vineESetup", "coreETimeline"},
                 {"vineSetups", "vineFSetup", "vineFFadeoutTimeline"},
                 {"vineSetups", "vineGSetup", "vineGFadeoutTimeline"},
-                {"vineSetups", "vineHSetup", "vineHFadeoutTimeline"},
+                // vineH is handled separately!
             };
 
             for (const auto& timeline_path: timeline_paths) {
@@ -46,7 +51,7 @@ namespace {
                 }
             }
 
-            // Fix coreG timeline
+            // Fix heart G (bottom right) timeline
             const auto core_g_death_timeline_go = il2cpp::unity::find_child(
                 scene_root_go,
                 std::vector<std::string> {
@@ -68,7 +73,51 @@ namespace {
                 }
             }
 
+            // Add no_cutscene timeline for heart H (bottom left) to prevent the boulder
+            // from teleporting
+            vine_h_change_state_on_condition_ref = il2cpp::WeakGCRef(
+                il2cpp::unity::get_component<app::ChangeStateOnCondition>(
+                    il2cpp::unity::find_child(
+                        scene_root_go,
+                        std::vector<std::string> {
+                            "vineSetups",
+                            "cores",
+                            "coreH",
+                            "weepingRidgeInfectionCore"
+                        }
+                    ),
+                    types::ChangeStateOnCondition::get_class()
+                )
+            );
+
+            const auto vine_h_setup_go = il2cpp::unity::find_child(
+                scene_root_go,
+                std::vector<std::string> {
+                    "vineSetups",
+                    "vineHSetup",
+                }
+            );
+            const auto vine_h_timeline_go = il2cpp::unity::find_child(vine_h_setup_go, "vineHFadeoutTimeline");
+            const auto vine_h_no_cutscene_timeline_go = il2cpp::unity::instantiate_object(vine_h_timeline_go);
+            UnityEngine::Object::set_name(reinterpret_cast<app::Object_1*>(vine_h_no_cutscene_timeline_go), il2cpp::string_new("vineHNoCutsceneTimeline(Rando)"));
+
+            const auto vine_h_rando_timeline = il2cpp::unity::get_component<app::MoonTimeline>(vine_h_no_cutscene_timeline_go, types::MoonTimeline::get_class());
+
+            vine_h_rando_timeline_ref = il2cpp::WeakGCRef(vine_h_rando_timeline);
+            vine_h_original_timeline_ref = il2cpp::WeakGCRef(il2cpp::unity::get_component<app::MoonTimeline>(vine_h_timeline_go, types::MoonTimeline::get_class()));
+
+            // Clean up our copied timeline
+            for (auto child_index: {21, 14, 13, 5, 4, 3, 2, 1, 0}) {
+                il2cpp::unity::destroy_object(il2cpp::unity::get_child(vine_h_no_cutscene_timeline_go, child_index));
+                il2cpp::invoke(vine_h_rando_timeline->fields.m_entityRecords, "RemoveAt", &child_index);
+            }
+
+            il2cpp::unity::set_parent(vine_h_no_cutscene_timeline_go, vine_h_setup_go);
+
+            // Setup the reactive effect
             effect = core::reactivity::watch_effect([] {
+                const auto disable_cutscenes = disable_cutscenes_state.get<bool>();
+
                 auto vines_it = vine_timelines.begin();
                 while (vines_it != vine_timelines.end()) {
                     if (!vines_it->is_valid()) {
@@ -76,10 +125,16 @@ namespace {
                         continue;
                     }
 
-                    (**vines_it)->fields._._PlayState_k__BackingField = disable_cutscenes_state.get<bool>()
+                    (**vines_it)->fields._._PlayState_k__BackingField = disable_cutscenes
                         ? app::AnimatorPlayState__Enum::None
                         : app::AnimatorPlayState__Enum::Stopped;
                     ++vines_it;
+                }
+
+                if (vine_h_change_state_on_condition_ref.has_value() && vine_h_change_state_on_condition_ref->is_valid()) {
+                    (**vine_h_change_state_on_condition_ref)->fields.StateChange->fields._._.StateData->fields._items->vector[0]->fields.m_transition->fields.m_volatileValue = disable_cutscenes
+                        ? **vine_h_rando_timeline_ref
+                        : **vine_h_original_timeline_ref;
                 }
 
                 if (vine_timelines.empty()) {
