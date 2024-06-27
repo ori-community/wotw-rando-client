32e424c28ed2f35a9dec05d12f323e368ca10a3c
Timo Schwarzer <timo@schwarzer.dev>
2024-06-27 21:18:12 +0200

    Fix Laser Shooting Elementals shooting in wrong angles after dying

--- projects/Randomizer/game/behaviour_changes/better_lasers.cpp
+++ projects/Randomizer/game/behaviour_changes/better_lasers.cpp
@@ -2,7 +2,10 @@
 
 #include <Modloader/app/methods/BlockableLaser.h>
 #include <Modloader/app/methods/Damage.h>
+#include <Modloader/app/methods/LaserShooterEntity.h>
+#include <Modloader/app/methods/LegacyRotateAnimator.h>
 #include <Modloader/app/types/Damage.h>
+#include <Modloader/app/types/LegacyRotateAnimator.h>
 #include <Modloader/interception_macros.h>
 #include <Modloader/modloader.h>
 
@@ -37,5 +40,31 @@ namespace {
     IL2CPP_INTERCEPT(BlockableLaser, void, OnEnable, (app::BlockableLaser * this_ptr)) {
         next::BlockableLaser::OnEnable(this_ptr);
         BlockableLaser::PerformLaserLogic(this_ptr);
+    }
+
+    // Fix Laser Shooter Elementals having a wrong laser rotation when
+    // dying while a laser is shot.
+    // This happens because the LegacyRotateAnimator does save and restore
+    // *global* rotation values instead of local ones, despite
+    // the laser being a child of the Elemental, which itself is rotated as well.
+    // The LegacyRotateAnimator component is not actually used, but causes
+    // internal crashes when being removed, due to the Pool Spawning system depending
+    // on its existence.
+    IL2CPP_INTERCEPT(LaserShooterEntity, void, OnAwake, (app::LaserShooterEntity * this_ptr)) {
+        next::LaserShooterEntity::OnAwake(this_ptr);
+
+        for (auto component: il2cpp::unity::get_components_in_children<app::LegacyRotateAnimator>(this_ptr, types::LegacyRotateAnimator::get_class(), true)) {
+            // Values other than 1.0f and 0.0f aren't used here, so I set it to
+            // -1.0f and short-circuit AnimateIt below if that value is set.
+            component->fields.RotateAxisFilter.z = -1.f;
+        }
+    }
+
+    IL2CPP_INTERCEPT(LegacyRotateAnimator, void, AnimateIt, (app::LegacyRotateAnimator* this_ptr, float value)) {
+        if (this_ptr->fields.RotateAxisFilter.z == -1.f) {
+            return;
+        }
+
+        next::LegacyRotateAnimator::AnimateIt(this_ptr, value);
     }
 } // namespace
