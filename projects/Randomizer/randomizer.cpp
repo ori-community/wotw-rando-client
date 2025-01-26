b4e5eaf17874d259dd15b89be68cd4db4af10c05
Satisha <satisharps@gmail.com>
2025-01-26 18:55:58 +0100

    First playable version of AP

--- projects/Randomizer/randomizer.cpp
+++ projects/Randomizer/randomizer.cpp
@@ -52,7 +52,6 @@ namespace randomizer {
         });
 
         auto seed_save_data = std::make_shared<seed::SaveSlotSeedMetaData>();
-        auto seed_meta = std::make_shared<seed::Seed::Data>()->info.meta;
 
         std::shared_ptr<seed::SeedSource> new_game_seed_source = std::make_shared<seed::EmptySeedSource>();
         std::string new_game_seed_content; // Set by spawning_and_preloading.cpp
@@ -145,6 +144,7 @@ namespace randomizer {
             game::shops::reset_shop_data();
             randomizer_seed.grant(core::api::uber_states::UberState(UberStateGroup::RandoEvents, 1), 0);
             seedgen_interop::request_states_update_on_next_reach_check();
+
             queue_reach_check();
             event_bus().trigger_event(RandomizerEvent::SeedLoadedPostGrant, EventTiming::Before);
             event_bus().trigger_event(RandomizerEvent::SeedLoadedPostGrant, EventTiming::After);
@@ -166,6 +166,13 @@ namespace randomizer {
             load_seed(false);
 
             core::api::game::player::shard_slots().set(3);
+
+            // Connect the AP client if AP is in the flags
+            auto seed_meta = randomizer_seed.info().meta;
+            if (std::ranges::find(seed_meta.flags, "AP") != seed_meta.flags.end()) {
+                std::string url = std::format("wss://{}:{}/", seed_meta.archipelago_address.value_or("archipelago.gg"), seed_meta.archipelago_port.value_or("38281"));
+                archipelago_client().connect(url, seed_meta.archipelago_slot_name.value_or("empty_slot_name"), seed_meta.archipelago_password.value_or(""));
+            }
         });
 
         auto on_fixed_update = core::api::game::event_bus().register_handler(GameEvent::FixedUpdate, EventTiming::Before, [](auto, auto) {
@@ -234,12 +241,6 @@ namespace randomizer {
         auto on_game_ready = modloader::event_bus().register_handler(ModloaderEvent::GameReady, [](auto) {
             monitor.display(&status);
             monitor.network_client(&client);
-
-            // Connect the AP client if AP is in the flags
-            if (std::find(seed_meta.flags.begin(), seed_meta.flags.end(), "AP") != seed_meta.flags.end()) {
-                std::string url = seed_meta.archipelago_address.value_or("archipelago.gg") + ":" + seed_meta.archipelago_port.value_or("38281");
-                archipelago_client().connect(url, seed_meta.archipelago_slot_name.value_or("empty_slot_name"), seed_meta.archipelago_password.value_or(""));
-            }
 
             universe.register_packet_handlers(client);
 
