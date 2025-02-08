bf80f5983ab0e0d88ffa31700679b4c4bac1351d
Satisha <satisharps@gmail.com>
2025-02-08 16:31:07 +0100

    Fix items on new save + improve code style

--- projects/Randomizer/archipelago/archipelago_save_meta.h
+++ projects/Randomizer/archipelago/archipelago_save_meta.h
@@ -1,12 +1,11 @@
 #pragma once
 
 #include <Core/save_meta/save_meta.h>
-#include <Randomizer/archipelago/archipelago_ids.h>
 
 namespace randomizer::archipelago {
     class ArchipelagoSaveData final : public core::save_meta::JsonSaveMetaSerializable {
     public:
-        int last_item_index;
+        int last_item_index {0};
 
         NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(ArchipelagoSaveData, last_item_index);
 
