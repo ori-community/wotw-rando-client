d11046f3ac54f96be9a371f59967c289ea9c8c5b
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2025-03-20 10:23:15 +0000

    Add gradient behind side map message.

--- projects/Randomizer/messages/map_message_side.cpp
+++ projects/Randomizer/messages/map_message_side.cpp
@@ -1,16 +1,41 @@
 #include <Core/api/game/game.h>
 #include <Core/api/messages/message_box.h>
+#include <Core/api/screen_position.h>
 #include <Modloader/app/methods/GameMapUI.h>
 #include <Modloader/app/methods/QuestIconsUI.h>
 #include <Modloader/app/methods/QuestsUI.h>
 #include <Modloader/app/types/AreaMapUI.h>
 #include <Modloader/app/types/QuestIconsUI.h>
 #include <Modloader/interception_macros.h>
+#include <Modloader/app/methods/ScalePositionForAspectRatio.h>
+#include <Modloader/app/types/ScalePositionForAspectRatio.h>
 #include <Randomizer/randomizer.h>
 
 using namespace app::classes;
 
 namespace {
+    std::unique_ptr<core::api::graphics::Sprite> map_side_background;
+    auto on_game_ready = modloader::event_bus().register_handler(ModloaderEvent::GameReady, [](auto) {
+        // We use Anchor::BottomLeft here because we are flipping the sprite.
+        map_side_background = std::make_unique<core::api::graphics::Sprite>(core::api::graphics::Sprite::Anchor::BottomLeft);
+        il2cpp::unity::set_object_name(map_side_background->get_game_object(), "map_side_background");
+
+        map_side_background->layer(Layer::UI);
+        map_side_background->local_scale({8, -8, 8});
+
+        auto top_left = get(core::api::screen_position::ScreenPosition::TopLeft, false);
+        top_left.z = -1.0f;
+        map_side_background->local_position(top_left);
+
+        map_side_background->texture(core::api::graphics::textures::get_texture("file:assets/textures/gradient_corner_bottom_left.png"));
+
+        const auto scale_position_component = il2cpp::unity::add_component<app::ScalePositionForAspectRatio>(
+            map_side_background->get_game_object(), types::ScalePositionForAspectRatio::get_class()
+        );
+        ScalePositionForAspectRatio::ctor(scale_position_component);
+        map_side_background->enabled(false);
+    });
+
     core::Property<std::string> map_message_side_text(text_id::SideMapMessage);
     std::shared_ptr<core::reactivity::ReactiveEffect> on_map_message_side_changed;
     std::shared_ptr<core::api::messages::MessageBox> box;
@@ -26,9 +51,9 @@ namespace {
             box = std::make_shared<core::api::messages::MessageBox>();
             box->set_name("map_message_side");
             box->coordinate_system().set(core::api::messages::CoordinateSystem::Relative);
-            box->position().set(0.05, 0.05, 0);
+            box->position().set(0.05f, 0.05f, -1.1f);
 
-            box->text_line_spacing().set(0.8);
+            box->text_line_spacing().set(0.8f);
             box->text_alignment().set(app::AlignmentMode__Enum::Left);
             box->box_horizontal_anchor().set(app::HorizontalAnchorMode__Enum::Left);
             box->box_vertical_anchor().set(app::VerticalAnchorMode__Enum::Top);
@@ -40,6 +65,7 @@ namespace {
         }
 
         box->show(true, false);
+        map_side_background->enabled(true);
     }
 
     void hide_map_message_side() {
@@ -48,6 +74,7 @@ namespace {
         }
 
         box->hide(true);
+        map_side_background->enabled(false);
     }
 
     IL2CPP_INTERCEPT(GameMapUI, void, UpdateQuests, (app::GameMapUI * this_ptr)) {
