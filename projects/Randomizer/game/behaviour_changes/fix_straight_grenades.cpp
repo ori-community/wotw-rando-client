cf6ac478acc4ba9194048e28513c21e305be1043
Timo Schwarzer <timo@schwarzer.dev>
2025-05-13 22:26:02 +0200

    Fix the grenade fractures fix

--- projects/Randomizer/game/behaviour_changes/fix_straight_grenades.cpp
+++ projects/Randomizer/game/behaviour_changes/fix_straight_grenades.cpp
@@ -1,6 +1,6 @@
 #include <Modloader/app/methods/SpiritGrenade.h>
-#include <Modloader/app/types/RigidbodySuspendable.h>
-#include <Modloader/app/methods/RigidbodySuspendable.h>
+#include <Modloader/app/types/Rigidbody.h>
+#include <Modloader/app/methods/UnityEngine/Rigidbody.h>
 #include <Modloader/interception_macros.h>
 #include <Modloader/windows_api/console.h>
 
@@ -19,8 +19,8 @@ namespace {
 
     IL2CPP_INTERCEPT(SpiritGrenade, void, SetTrajectory, (app::SpiritGrenade * this_ptr, app::Vector2 speed)) {
         if (!this_ptr->fields.Bashable && !this_ptr->fields.Fractures) {
-            const auto rigid_body_suspendable = il2cpp::unity::get_component<app::RigidbodySuspendable>(this_ptr, types::RigidbodySuspendable::get_class());
-            RigidbodySuspendable::Resume(rigid_body_suspendable);
+            const auto rigid_body = il2cpp::unity::get_component<app::Rigidbody>(this_ptr, types::Rigidbody::get_class());
+            UnityEngine::Rigidbody::set_isKinematic(rigid_body, false);
         }
 
         next::SpiritGrenade::SetTrajectory(this_ptr, speed);
