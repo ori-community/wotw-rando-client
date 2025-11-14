48f66d4d19b8c206a4d3fd90eda08884e9970791
Timo Schwarzer <timo@schwarzer.dev>
2025-11-14 17:19:55 +0100

    Fix Voice of the Forest not being granted under certain circumstances

--- projects/Randomizer/game/behaviour_changes/fix_kwoloks_hollow_cutscene_skip.cpp
+++ projects/Randomizer/game/behaviour_changes/fix_kwoloks_hollow_cutscene_skip.cpp
@@ -1,19 +1,23 @@
-#include <iostream>
 #include <optional>
 
 #include <Core/api/scenes/scene_load.h>
 #include <Modloader/app/methods/QuestsController.h>
 #include <Modloader/app/methods/SkipCutscene.h>
+#include <Modloader/app/methods/Moon/Timeline/MoonTimeline.h>
 #include <Modloader/app/types/QuestsController.h>
 #include <Modloader/app/types/SkipCutscene.h>
+#include <Modloader/app/types/MoonTimeline.h>
 #include <Modloader/modloader.h>
+
+#include "Core/api/game/player.h"
 
 using namespace app::classes;
 
 namespace {
     std::optional<il2cpp::WeakGCRef<app::SkipCutscene>> skip_cutscene;
+    std::optional<il2cpp::WeakGCRef<app::MoonTimeline>> emerge_cutscene_b_timeline;
 
-    void on_scene_load(const core::api::scenes::SceneLoadEventMetadata* metadata, const std::string&) {
+    void on_meeting_kwolok_scene_load(const core::api::scenes::SceneLoadEventMetadata* metadata, const std::string&) {
         if (metadata->state != app::SceneState__Enum::Loaded) {
             return;
         }
@@ -32,6 +36,60 @@ namespace {
         }
     }
 
+    void on_kwoloks_cavern_throne_room_scene_load(const core::api::scenes::SceneLoadEventMetadata* metadata, const std::string&) {
+        if (metadata->state != app::SceneState__Enum::Loaded) {
+            return;
+        }
+
+        auto scene_root_go = il2cpp::unity::get_game_object(metadata->scene->fields.SceneRoot);
+
+        auto get_pickup_on_condition_go = il2cpp::unity::find_child(
+            scene_root_go,
+            std::vector<std::string>{
+                "kwolokSetup",
+                "getPickupOnCondition",
+            }
+        );
+
+        if (il2cpp::unity::is_valid(get_pickup_on_condition_go)) {
+            il2cpp::unity::destroy_object(get_pickup_on_condition_go);
+        }
+
+        auto emerge_cutscene_b_timeline_go = il2cpp::unity::find_child(
+            scene_root_go,
+            std::vector<std::string>{
+                "timelineSequences",
+                "emergeCutsceneB",
+            }
+        );
+
+        if (il2cpp::unity::is_valid(emerge_cutscene_b_timeline_go)) {
+            emerge_cutscene_b_timeline = il2cpp::WeakGCRef(il2cpp::unity::get_component<app::MoonTimeline>(emerge_cutscene_b_timeline_go, types::MoonTimeline::get_class()));
+        }
+    }
+
+    /**
+     * This function replicates the behavior of
+     * kwoloksCavernThroneRoom/kwolokSetup/getPickupOnCondition without
+     * the condition that kwolokGroupDescriptor.cleanseWellspringQuestUberState needs to be 1
+     * to give the pickup.
+     */
+    void give_voice_if_not_given() {
+        core::api::uber_states::UberState forests_voice_state(46462, 59806);
+
+        if (forests_voice_state.get<bool>()) {
+            return;
+        }
+
+        core::api::game::player::max_energy().add(1.f);
+        core::api::game::player::max_health().add(10.f);
+        core::api::game::player::refill_energy();
+        core::api::game::player::refill_health();
+        forests_voice_state.set(true);
+    }
+
+    // Give Voice of the Forest when skipping the cutscene.
+    // Also make sure the quests are being advanced.
     IL2CPP_INTERCEPT(void, SkipCutscene, OnSkip, app::SkipCutscene* this_ptr) {
         next::SkipCutscene::OnSkip(this_ptr);
 
@@ -51,9 +109,22 @@ namespace {
 
             QuestsController::CompleteQuest(quests_controller, kwolok_quest);
             QuestsController::AssignQuest(quests_controller, wellspring_quest, true, false, true);
+            give_voice_if_not_given();
+        }
+    }
+
+    // Give Voice of the Forest when watching the full cutscene
+    IL2CPP_INTERCEPT(void, Moon::Timeline::MoonTimeline, OnStopPlayback, app::MoonTimeline* this_ptr) {
+        next::Moon::Timeline::MoonTimeline::OnStopPlayback(this_ptr);
+
+        if (emerge_cutscene_b_timeline.has_value() && emerge_cutscene_b_timeline->is_valid() && **emerge_cutscene_b_timeline == this_ptr) {
+            give_voice_if_not_given();
         }
     }
 
     [[maybe_unused]]
-    auto on_meeting_kwolok_scene_load = core::api::scenes::single_event_bus().register_handler("meetingKwolokMaster", on_scene_load);
+    auto on_meeting_kwolok_scene_load_handle = core::api::scenes::single_event_bus().register_handler("meetingKwolokMaster", on_meeting_kwolok_scene_load);
+
+    [[maybe_unused]]
+    auto on_kwoloks_cavern_throne_room_scene_load_handle = core::api::scenes::single_event_bus().register_handler("kwoloksCavernThroneRoom", on_kwoloks_cavern_throne_room_scene_load);
 } // namespace
