b03857f0c9d4cb603fdc946849358648c366f671
Timo Schwarzer <timo@schwarzer.dev>
2025-11-15 15:11:33 +0100

    Move most states out of BingoState to RandoStats and refactor enemy kill tracking

--- projects/Randomizer/stats/enemies.cpp
+++ projects/Randomizer/stats/enemies.cpp
@@ -13,6 +13,7 @@
 #include <Modloader/il2cpp_helpers.h>
 
 #include <Common/event_bus.h>
+#include <Common/vx.h>
 #include <Core/api/game/death_listener.h>
 #include <Core/api/uber_states/uber_state.h>
 
@@ -22,31 +23,31 @@ namespace core::api::death_listener {
 
     namespace {
         std::unordered_map<app::DamageType__Enum, UberState> damage_type_map{
-            {app::DamageType__Enum::Sword, UberState(UberStateGroup::BingoState, 11)},
-            {app::DamageType__Enum::Hammer, UberState(UberStateGroup::BingoState, 12)},
-            {app::DamageType__Enum::Bow, UberState(UberStateGroup::BingoState, 13)},
-            {app::DamageType__Enum::SpiritSpear, UberState(UberStateGroup::BingoState, 14)},
-            {app::DamageType__Enum::SpiritSentry, UberState(UberStateGroup::BingoState, 15)},
-            {app::DamageType__Enum::Blaze, UberState(UberStateGroup::BingoState, 16)},
-            {app::DamageType__Enum::Grenade, UberState(UberStateGroup::BingoState, 17)},
-            {app::DamageType__Enum::Chakram, UberState(UberStateGroup::BingoState, 19)},
-            {app::DamageType__Enum::ChargeJump, UberState(UberStateGroup::BingoState, 20)},
-            {app::DamageType__Enum::Glow, UberState(UberStateGroup::BingoState, 21)},
-            {app::DamageType__Enum::Projectile, UberState(UberStateGroup::BingoState, 22)},
-            {app::DamageType__Enum::Water, UberState(UberStateGroup::BingoState, 23)},
+            {app::DamageType__Enum::Sword, UberState(UberStateGroup::RandoStats, 11)},
+            {app::DamageType__Enum::Hammer, UberState(UberStateGroup::RandoStats, 12)},
+            {app::DamageType__Enum::Bow, UberState(UberStateGroup::RandoStats, 13)},
+            {app::DamageType__Enum::SpiritSpear, UberState(UberStateGroup::RandoStats, 14)},
+            {app::DamageType__Enum::SpiritSentry, UberState(UberStateGroup::RandoStats, 15)},
+            {app::DamageType__Enum::Blaze, UberState(UberStateGroup::RandoStats, 16)},
+            {app::DamageType__Enum::Grenade, UberState(UberStateGroup::RandoStats, 17)},
+            {app::DamageType__Enum::Chakram, UberState(UberStateGroup::RandoStats, 19)},
+            {app::DamageType__Enum::ChargeJump, UberState(UberStateGroup::RandoStats, 20)},
+            {app::DamageType__Enum::Glow, UberState(UberStateGroup::RandoStats, 21)},
+            {app::DamageType__Enum::Projectile, UberState(UberStateGroup::RandoStats, 22)},
+            {app::DamageType__Enum::Water, UberState(UberStateGroup::RandoStats, 23)},
         };
 
-        using enemy_list = std::vector<UberState>;
-        using enemy_function = std::function<std::vector<UberState>(app::EnemyEntity* entity)>;
+        using enemy_list_t = std::vector<UberState>;
+        using enemy_function_t = std::function<std::vector<UberState>(app::EnemyEntity* entity)>;
 
         std::vector<UberState> skeeto_check(app::EnemyEntity* entity) {
             const auto skeeto = reinterpret_cast<app::SkeetoEntity*>(entity);
             switch (skeeto->fields.m_type) {
                 case app::SkeetoEntity_SkeetoType__Enum::Kamikaze: {
-                    return {UberState(UberStateGroup::BingoState, 41), UberState(UberStateGroup::BingoState, 45)};
+                    return {UberState(UberStateGroup::RandoStats, 41), UberState(UberStateGroup::RandoStats, 45)};
                 }
                 default: {
-                    return {UberState(UberStateGroup::BingoState, 41)};
+                    return {UberState(UberStateGroup::RandoStats, 41)};
                 }
             }
         }
@@ -54,41 +55,45 @@ namespace core::api::death_listener {
         std::vector<UberState> worm_check(app::EnemyEntity* entity) {
             const auto worm = reinterpret_cast<app::SandWormEntity*>(entity);
             if (worm->fields.WormHabitat == app::SandWormEntity_Habitat__Enum::Sand) {
-                return enemy_list{};
+                return enemy_list_t{};
             } else {
-                return enemy_list{UberState(UberStateGroup::BingoState, 44)};
+                return enemy_list_t{UberState(UberStateGroup::RandoStats, 44)};
             }
         }
 
-        std::unordered_map<void*, std::variant<enemy_list, enemy_function>>& enemy_type_map() {
-            static std::unordered_map<void*, std::variant<enemy_list, enemy_function>> inner_enemy_type_map = {
-                {types::MinerEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 40)}},
+        std::unordered_map<void*, std::variant<enemy_list_t, enemy_function_t>>& enemy_type_map() {
+            static std::unordered_map<void*, std::variant<enemy_list_t, enemy_function_t>> inner_enemy_type_map = {
+                {types::MinerEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 40)}},
                 {types::SkeetoEntity::get_class(), skeeto_check},
-                {types::TentacleEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 42)}},
-                {types::SpikeSlugEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 43)}},
-                {types::DropSlugEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 43), UberState(UberStateGroup::BingoState, 45)}},
-                {types::PiranhaEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 44)}},
+                {types::TentacleEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 42)}},
+                {types::SpikeSlugEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 43)}},
+                {types::DropSlugEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 43), UberState(UberStateGroup::RandoStats, 45)}},
+                {types::PiranhaEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 44)}},
                 {types::SandWormEntity::get_class(), worm_check},
-                {types::GasballEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 41), UberState(UberStateGroup::BingoState, 45)}},
-                {types::KamikazeLizardEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 45)}},
-                {types::BombSlugEntity::get_class(), enemy_list{UberState(UberStateGroup::BingoState, 43), UberState(UberStateGroup::BingoState, 45)}}
+                {types::GasballEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 41), UberState(UberStateGroup::RandoStats, 45)}},
+                {types::KamikazeLizardEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 45)}},
+                {types::BombSlugEntity::get_class(), enemy_list_t{UberState(UberStateGroup::RandoStats, 43), UberState(UberStateGroup::RandoStats, 45)}}
             };
 
             return inner_enemy_type_map;
         }
 
-        UberState enemy_state(UberStateGroup::BingoState, 10);
+        UberState kills_state(UberStateGroup::RandoStats, 10);
 
-        void handle_enemy_stats(Death death, EventTiming timing) {
-            enemy_state.set(enemy_state.get() + 1);
+        void handle_enemy_stats(Death death, EventTiming) {
+            kills_state.set(kills_state.get() + 1);
             const auto enemy_entity = il2cpp::unity::get_component<app::EnemyEntity>(death.game_object, types::EnemyEntity::get_class());
             const auto enemy_state_entry = enemy_type_map()[enemy_entity->klass];
-            enemy_list states;
-            if (enemy_state_entry.index() == 0) {
-                states = std::get<0>(enemy_state_entry);
-            } else {
-                states = std::get<1>(enemy_state_entry)(enemy_entity);
-            }
+            enemy_list_t states;
+
+            enemy_state_entry | vx::match {
+                [&](const enemy_list_t& list) {
+                    states = list;
+                },
+                [&](const enemy_function_t& function) {
+                    states = function(enemy_entity);
+                }
+            };
 
             for (auto state: states) {
                 state.set(state.get() + 1);
