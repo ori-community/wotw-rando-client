8ff14a20d02981a49458a78c578f1807b43e8392
Timo Schwarzer <timo@schwarzer.dev>
2025-11-17 11:59:51 +0100

    Fix Strength of the Forest not giving Wisp resources when skipping the cutscene

--- projects/Randomizer/features/cutscene_skips/kwolok_boss_farewell.cpp
+++ projects/Randomizer/features/cutscene_skips/kwolok_boss_farewell.cpp
@@ -2,18 +2,22 @@
 #include <Modloader/app/types/MoonTimeline.h>
 #include <Modloader/il2cpp_helpers.h>
 
-#include "custom_cutscene_skips.h"
 #include <Common/event_bus.h>
 #include <Core/api/scenes/scene_load.h>
 #include <Core/api/uber_states/uber_state.h>
 #include <Core/utils/misc.h>
+#include <Modloader/app/methods/Moon/Timeline/MoonTimeline.h>
 #include <Modloader/modloader.h>
+#include <Randomizer/features/cutscene_skips/custom_cutscene_skips.h>
+
+#include <Core/api/game/player.h>
 
 using namespace utils;
 using namespace app::classes;
 
 namespace {
     ObjectReference<app::MoonTimeline> kwolok_boss_farewell_timeline;
+    core::api::uber_states::UberState pools_wisp_state(945, 49747);
 
     void on_scene_load(core::api::scenes::SceneLoadEventMetadata* metadata) {
         if (metadata->state != app::SceneState__Enum::Loaded) {
@@ -36,6 +40,22 @@ namespace {
         }
     }
 
+    /**
+     * This function replicates the behavior of
+     * kwolokBossSetup/getPickupOnCondition.
+     */
+    void give_strength_if_not_given() {
+        if (pools_wisp_state.get<bool>()) {
+            return;
+        }
+
+        core::api::game::player::max_energy().add(1.f);
+        core::api::game::player::max_health().add(10.f);
+        core::api::game::player::refill_energy();
+        core::api::game::player::refill_health();
+        pools_wisp_state.set(true);
+    }
+
     bool skip_available() {
         return core::api::scenes::scene_is_loaded("kwolokBossGetWisp") &&
             kwolok_boss_farewell_timeline.is_valid() &&
@@ -44,9 +64,7 @@ namespace {
 
     void skip_invoke() {
         Moon::Timeline::TimelineEntity::StopPlayback(reinterpret_cast<app::TimelineEntity*>(kwolok_boss_farewell_timeline.ptr));
-
-        core::api::uber_states::UberState pools_wisp_state(static_cast<UberStateGroup>(945), 49747);
-        pools_wisp_state.set(1.0);
+        give_strength_if_not_given();
     }
 
     auto on_scene_load_handle = core::api::scenes::event_bus().register_handler(&on_scene_load);
