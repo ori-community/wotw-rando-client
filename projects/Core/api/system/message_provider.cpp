57bec8a9668861f4b806825b96062b9fc48e252f
Timo Schwarzer <timo@schwarzer.dev>
2025-12-31 00:10:50 +0100

    Fix crash when creating message providers from invalid cache

--- projects/Core/api/system/message_provider.cpp
+++ projects/Core/api/system/message_provider.cpp
@@ -5,13 +5,15 @@ namespace core::api::system {
     using namespace app::classes;
 
     app::TranslatedMessageProvider* instantiate_empty_translated_message_provider() {
-        static app::TranslatedMessageProvider* empty_instance_cache = nullptr;
+        static std::optional<il2cpp::WeakGCRef<app::TranslatedMessageProvider>> empty_instance_cache = std::nullopt;
 
-        if (!il2cpp::unity::is_valid(empty_instance_cache)) {
-            empty_instance_cache = types::TranslatedMessageProvider::create();
-            TranslatedMessageProvider::ctor(empty_instance_cache);
+        if (!empty_instance_cache.has_value() || !empty_instance_cache->is_valid()) {
+            const auto empty_instance = types::TranslatedMessageProvider::create();
+            TranslatedMessageProvider::ctor(empty_instance);
+            empty_instance_cache = il2cpp::WeakGCRef(empty_instance);
+            return il2cpp::unity::instantiate_object(empty_instance);
         }
 
-        return il2cpp::unity::instantiate_object(empty_instance_cache);
+        return il2cpp::unity::instantiate_object(**empty_instance_cache);
     }
 } // namespace core::api::system
