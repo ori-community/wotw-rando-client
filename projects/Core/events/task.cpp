9496c0bda5e59f39a9aeb642dcc17bf78f180652
Timo Schwarzer <timo@schwarzer.dev>
2026-02-09 22:56:08 +0100

    Make schedule_task thread safe

--- projects/Core/events/task.cpp
+++ projects/Core/events/task.cpp
@@ -4,6 +4,8 @@
 #include <Modloader/app/methods/GameController.h>
 #include <Modloader/interception_macros.h>
 #include <Modloader/modloader.h>
+#include <mutex>
+#include <utility>
 
 using namespace modloader;
 using namespace app::classes;
@@ -14,47 +16,47 @@ namespace core::events {
         std::function<void()> callback;
     };
 
-    bool is_updating_tasks = false;
     float timer = 0.f;
-    std::vector<Task> task_buffer;
+    std::mutex tasks_mutex;
     std::vector<Task> tasks;
 
     void schedule_task(float seconds, std::function<void()> task) {
-        if (is_updating_tasks) {
-            task_buffer.push_back(Task{ seconds, task });
-        } else {
-            seconds += timer;
-            auto it = tasks.rbegin();
-            // We use a reverse iterator as we assume later tasks take a longer time then earlier ones.
-            while (it != tasks.rend() && it->seconds <= seconds) {
-                ++it;
-            }
+        std::lock_guard _(tasks_mutex);
 
-            tasks.insert(it.base(), Task{ seconds, task });
+        seconds += timer;
+        auto it = tasks.rbegin();
+
+        // We use a reverse iterator as we assume later tasks take a longer time then earlier ones.
+        while (it != tasks.rend() && it->seconds <= seconds) {
+            ++it;
         }
+
+        tasks.insert(it.base(), Task{ seconds, std::move(task) });
     }
 
     void schedule_task_for_next_update(std::function<void()> task) {
-        schedule_task(-timer, task);
+        schedule_task(-timer, std::move(task));
     }
 
     auto before_unity_update = core::api::game::event_bus().register_handler(GameEvent::UnityUpdateLoop, EventTiming::After, [](auto, auto) {
         if (!tasks.empty()) {
-            modloader::ScopedSetter updater(is_updating_tasks, true);
-            is_updating_tasks = true;
             timer += api::game::fixed_delta_time();
-            while (!tasks.empty() && tasks.back().seconds <= timer) {
-                tasks.back().callback();
-                tasks.pop_back();
+
+            std::vector<Task> tasks_to_run;
+
+            {
+                std::lock_guard _(tasks_mutex);
+                while (!tasks.empty() && tasks.back().seconds <= timer) {
+                    tasks_to_run.push_back(tasks.back());
+                    tasks.pop_back();
+                }
+            }
+
+            for (auto& task_to_run: tasks_to_run) {
+                task_to_run.callback();
             }
         } else {
             timer = 0.f;
         }
-
-        for (auto const& task : task_buffer) {
-            schedule_task(task.seconds, task.callback);
-        }
-
-        task_buffer.clear();
     });
 } // namespace core::events
