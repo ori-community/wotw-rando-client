c0c7df4832f37f42bbde4998ba7260c62e590f59
Timo Schwarzer <timo@schwarzer.dev>
2026-05-05 22:55:13 +0200

    Don't extrapolate player icons

--- projects/Randomizer/map/player_icon.cpp
+++ projects/Randomizer/map/player_icon.cpp
@@ -7,17 +7,11 @@
 namespace randomizer::map::player_icon {
     using namespace app::classes;
 
-    constexpr float MAX_EXTRAPOLATION_TIME = 0.5f;
-
     PlayerIcon::PlayerIcon() {
-        m_on_before_update_droppable = core::api::game::event_bus().register_handler(GameEvent::UnityUpdateLoop, EventTiming::Before, [this](auto, auto) {
-            on_before_update();
-        });
-
         m_map_icon = std::make_shared<icons::MapIcon>(
             icons::MapIcon::Type::OriPlayer,
             "Player",
-            m_live_position,
+            m_live_position.value_or(app::Vector2{}),
             [this](filter::MapFilter) {
                 return visible.get()
                     ? icons::MapIcon::Visibilities::visible
@@ -33,35 +27,11 @@ namespace randomizer::map::player_icon {
     }
 
     void PlayerIcon::set_live_position(const app::Vector2& position) {
-        const auto now = std::chrono::high_resolution_clock::now();
-
-        m_velocity = [&] -> std::optional<app::Vector2> {
-            if (m_live_position_time.has_value()) {
-                const auto time_delta = duration_cast<std::chrono::microseconds>(now - *m_live_position_time).count() / 1000000.f;
-
-                if (time_delta > 0.f) {
-                    const auto position_delta = position - m_live_position;
-                    return position_delta / time_delta;
-                }
-            }
-
-            return std::nullopt;
-        }().value_or(app::Vector2{});
-
         m_live_position = position;
-        m_live_position_time = now;
-        m_extrapolation_time = 0.f;
         m_map_icon->world_position.set(position);
     }
 
     void PlayerIcon::set_color(const SolidColor& color) const {
         m_map_icon->label_color.set({color.r, color.g, color.b, 1.f});
-    }
-
-    void PlayerIcon::on_before_update() {
-        if (m_extrapolation_time < MAX_EXTRAPOLATION_TIME) {
-            m_extrapolation_time = std::min(MAX_EXTRAPOLATION_TIME, m_extrapolation_time + UnityEngine::Time::get_deltaTime());
-            m_map_icon->world_position.set(m_live_position + m_velocity * m_extrapolation_time);
-        }
     }
 } // namespace randomizer::map::player_icon
--- projects/Randomizer/map/player_icon.h
+++ projects/Randomizer/map/player_icon.h
@@ -1,6 +1,4 @@
 #pragma once
-#include <Modloader/app/structs/Vector2.h>
-#include <Common/droppable.h>
 #include <Randomizer/map/map_icons.h>
 
 namespace randomizer::map::player_icon {
@@ -17,12 +15,6 @@ namespace randomizer::map::player_icon {
     private:
         icons::MapIcon::ptr_t m_map_icon;
 
-        common::Droppable::ptr_t m_on_before_update_droppable;
-        app::Vector2 m_live_position{};
-        std::optional<std::chrono::time_point<std::chrono::steady_clock>> m_live_position_time = std::nullopt;
-        app::Vector2 m_velocity{};
-        float m_extrapolation_time = 0.f;
-
-        void on_before_update();
+        std::optional<app::Vector2> m_live_position = std::nullopt;
     };
 }
