c530ead6850249345b4ae84f230268b568ec9b29
Timo Schwarzer <timo@schwarzer.dev>
2026-05-06 23:29:51 +0200

    Disallow get_MapDisplayPosition to leave the map boundaries

--- projects/Randomizer/map/map_pan.cpp
+++ projects/Randomizer/map/map_pan.cpp
@@ -1,20 +1,20 @@
 #include <Core/api/game/game.h>
 #include <Core/api/uber_states/uber_state.h>
 #include <Core/settings.h>
-#include <Core/text/text_database.h>
 #include <Modloader/app/methods/AreaMapNavigation.h>
+#include <Modloader/app/methods/SeinCharacter.h>
 #include <Modloader/app/methods/UnityEngine/AnimationCurve.h>
 #include <Modloader/interception_macros.h>
 #include <Modloader/modloader.h>
 #include <Randomizer/randomizer.h>
+#include <Core/api/game/player.h>
 
-#include "Core/api/game/player.h"
 
 namespace {
     using namespace app::classes;
 
     auto is_handling_map_scrolling = false;
-    app::Vector2 last_non_void_player_map_position{};
+    app::Vector3 last_non_void_player_map_position{};
 
     IL2CPP_INTERCEPT_WITH_ORDER(0, float, UnityEngine::AnimationCurve, Evaluate, app::AnimationCurve* this_ptr, float time) {
         if (is_handling_map_scrolling) {
@@ -29,23 +29,24 @@ namespace {
         next::AreaMapNavigation::HandleMapScrolling(this_ptr);
     }
 
-    [[maybe_unused]]
-    auto on_update = core::api::game::event_bus().register_handler(GameEvent::FixedUpdate, EventTiming::After, [](auto, auto) {
-        const auto player_position = core::api::game::player::get_map_position();
+    IL2CPP_INTERCEPT(app::Vector3, SeinCharacter, get_MapDisplayPosition, app::SeinCharacter* this_ptr) {
+        const auto original_map_position = next::SeinCharacter::get_MapDisplayPosition(this_ptr);
 
         if (  // Approximately the map rectangle
-            player_position.x > -2197 &&
-            player_position.x < 2580 &&
-            player_position.y > -4800 &&
-            player_position.y < -3300
+            original_map_position.x > -2197 &&
+            original_map_position.x < 2580 &&
+            original_map_position.y > -4800 &&
+            original_map_position.y < -3300
         ) {
-            last_non_void_player_map_position = core::api::game::player::get_map_position();
+            last_non_void_player_map_position = original_map_position;
         }
-    });
 
-    IL2CPP_INTERCEPT(void, AreaMapNavigation, SetLocationPlayer, app::AreaMapNavigation* this_ptr) {
-        this_ptr->fields.m_scrollPosition = last_non_void_player_map_position;
-        this_ptr->fields.m_fromPosition = last_non_void_player_map_position;
-        this_ptr->fields.m_toPosition = last_non_void_player_map_position;
+        return last_non_void_player_map_position;
     }
+
+    [[maybe_unused]]
+    auto on_update = core::api::game::event_bus().register_handler(GameEvent::FixedUpdate, EventTiming::After, [](auto, auto) {
+        // To force update last_non_void_player_map_position
+        core::api::game::player::get_map_position();
+    });
 }
