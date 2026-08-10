edf94107ef3b8b23d52c7a7cff8b466d09e5e78d
Timo Schwarzer <timo@schwarzer.dev>
2026-08-11 00:06:11 +0200

    Don't crash when confined asset is not found

--- src/Modloader/fs.cpp
+++ src/Modloader/fs.cpp
@@ -4,7 +4,15 @@
 namespace modloader::fs {
     std::optional<std::filesystem::path> get_confined_asset_path(const std::filesystem::path& relative_path) {
         const auto canonical_assets_path = std::filesystem::canonical(modloader::get_install_data_path("client/assets"));
-        auto canonical_asset_path = std::filesystem::canonical(canonical_assets_path / relative_path);
+
+        const auto full_path = canonical_assets_path / relative_path;
+
+        if (!std::filesystem::exists(full_path)) {
+            modloader::error("fs", std::format("Asset not found: {}", full_path.string()));
+            return std::nullopt;
+        }
+
+        auto canonical_asset_path = std::filesystem::canonical(full_path);
 
         // Check whether canonical_asset_path is inside canonical_assets_path
         const auto mismatch_pair = std::mismatch(canonical_asset_path.begin(), canonical_asset_path.end(), canonical_assets_path.begin(), canonical_assets_path.end());
