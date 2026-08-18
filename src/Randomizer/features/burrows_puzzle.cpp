6d5f53a533fc4d31cdcc0053828292df28caf832
Timo Schwarzer <timo@schwarzer.dev>
2026-08-18 23:22:14 +0200

    Refactor burrows_puzzle

--- src/Randomizer/features/burrows_puzzle.cpp
+++ src/Randomizer/features/burrows_puzzle.cpp
@@ -1,40 +1,36 @@
+#include <Core/api/audio.h>
 #include <Core/api/scenes/scene_load.h>
+#include <Core/events/task.h>
 #include <Core/property/reactivity.h>
 #include <Modloader/app/methods/Moon/Timeline/TimelineEntity.h>
+#include <Modloader/app/methods/SwitchSeriesPuzzle.h>
+#include <Modloader/app/methods/SwitchSeriesPuzzleSwitchSetupHolder.h>
 #include <Modloader/il2cpp_helpers.h>
-#include <Modloader/modloader.h>
-#include "Modloader/app/methods/SwitchSeriesPuzzle.h"
-#include "Modloader/app/methods/SwitchSeriesPuzzleSwitchSetupHolder.h"
 #include <queue>
-
-#include "Core/api/audio.h"
-#include "Core/events/task.h"
-
-enum class Bell {
-    Left,
-    Mid,
-    Right,
-};
 
 namespace {
     using namespace app::classes;
 
-    std::vector bell_states{1919, 24796, 7459};
-    std::vector<Bell> desired_sequence_A = {Bell::Left, Bell::Mid, Bell::Right, Bell::Right, Bell::Left, Bell::Mid, Bell::Left};
-    std::vector<Bell> desired_sequence_B = {Bell::Left, Bell::Mid, Bell::Left, Bell::Right, Bell::Right, Bell::Mid, Bell::Left};
-    std::deque<Bell> bell_queue_A;
-    std::deque<Bell> bell_queue_B;
+    enum class Bell {
+        Left,
+        Mid,
+        Right,
+    };
+
+    std::vector desired_sequence_burrows = {Bell::Left, Bell::Mid, Bell::Right, Bell::Right, Bell::Left, Bell::Mid, Bell::Left};
+    std::vector desired_sequence_tree = {Bell::Left, Bell::Mid, Bell::Left, Bell::Right, Bell::Right, Bell::Mid, Bell::Left};
+    std::deque<Bell> bell_queue_burrows;
+    std::deque<Bell> bell_queue_tree;
 
     void check_switch_puzzle(
         app::SwitchSeriesPuzzle* switch_series_puzzle,
         const app::SwitchSeriesPuzzleSetupData* activated_state,
-        const core::api::uber_states::UberState event_uberstate,
+        const core::api::uber_states::UberState& event_uber_state,
         const std::vector<Bell>& desired_sequence,
         std::deque<Bell>& bell_queue
     ) {
-
         // do nothing if puzzle already completed
-        if (event_uberstate.get<bool>()) {
+        if (event_uber_state.get<bool>()) {
             return;
         }
 
@@ -45,13 +41,20 @@ namespace {
         }
         const auto changed_uber_state = core::api::uber_states::UberState(reinterpret_cast<app::IUberState*>(changed_state));
 
-        // get index and enum of bell
-        const auto it = std::ranges::find(bell_states.begin(), bell_states.end(), changed_uber_state.state());
-        if (it == bell_states.end()) {
-            return;
+        Bell bell;
+        switch (changed_uber_state.state()) {
+            case 1919: {
+                bell = Bell::Left;
+            } break;
+            case 24796: {
+                bell = Bell::Mid;
+            } break;
+            case 7459: {
+                bell = Bell::Right;
+            } break;
+            default:
+                return;
         }
-        int bell_index = it - bell_states.begin();
-        Bell bell = static_cast<Bell>(bell_index);
 
         // add new bell to queue and pop first
         bell_queue.push_back(bell);
@@ -61,7 +64,7 @@ namespace {
 
         if (std::ranges::equal(bell_queue, desired_sequence)) {
             // mark puzzle as solved and play timeline
-            event_uberstate.set(true);
+            event_uber_state.set(true);
             SwitchSeriesPuzzleSwitchSetupHolder::StopTimelines(switch_series_puzzle->fields.SetupStates);
             Moon::Timeline::TimelineEntity::StartPlayback_1(reinterpret_cast<app::TimelineEntity*>(activated_state->fields.m_transition));
 
@@ -75,7 +78,6 @@ namespace {
     }
 
     IL2CPP_INTERCEPT_WITH_ORDER(10, void, SwitchSeriesPuzzle, Apply, app::SwitchSeriesPuzzle* this_ptr, app::UberStateApplyContext__Enum context) {
-
         if (context != app::UberStateApplyContext__Enum::ValueChanged) {
             return;
         }
@@ -84,11 +86,12 @@ namespace {
         if (activated_state == nullptr) {
             return;
         }
+
         auto path = il2cpp::unity::get_path(this_ptr);
         if (path == "howlsOriginEntrance/interactives/switchSequencePuzzleA") {
-            check_switch_puzzle(this_ptr, activated_state, core::api::uber_states::UberState(24922, 13349), desired_sequence_A, bell_queue_A);
+            check_switch_puzzle(this_ptr, activated_state, core::api::uber_states::UberState(24922, 13349), desired_sequence_burrows, bell_queue_burrows);
         } else if (path == "howlsOriginEntrance/interactives/switchSequencePuzzleB") {
-            check_switch_puzzle(this_ptr, activated_state, core::api::uber_states::UberState(24922, 59146), desired_sequence_B, bell_queue_B);
+            check_switch_puzzle(this_ptr, activated_state, core::api::uber_states::UberState(24922, 59146), desired_sequence_tree, bell_queue_tree);
         }
     }
 
